b871289960085ef9cd345a56bf6eedc6707b44fe
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-09 17:56:18 -0300

    I remove links, timestamp.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 import LandingPage from '@/components/LandingPage';
-import { DEFAULT_FRAME, VERCEL_URL } from '@/lib/consts';
+import { DEFAULT_FRAME, DESCRIPTION, TITLE, VERCEL_URL } from '@/lib/consts';
 
 const frameMetadata = { ...getFrameMetadata(DEFAULT_FRAME), 'of:accepts:xmtp': '2024-02-01' };
 
 export const metadata: Metadata = {
-  title: 'viem snapshot',
-  description: 'viem snapshot',
+  title: TITLE,
+  description: DESCRIPTION,
   openGraph: {
-    title: 'viem snapshot',
-    description: 'viem snapshot',
+    title: TITLE,
+    description: DESCRIPTION,
     images: [`https://${VERCEL_URL}/api/og`],
   },
   other: {
