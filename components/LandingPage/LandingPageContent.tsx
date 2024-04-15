5fca33a2e13d3944f99f52e7de4aa5566f42e3ed
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-15 13:37:22 -0300

    I cleanup logic for SignInButton.

--- components/LandingPage/LandingPageContent.tsx
+++ components/LandingPage/LandingPageContent.tsx
@@ -2,7 +2,7 @@ import MadeBySweets from '../MadeBySweets';
 import LandingPageHeader from './LandingPageHeader';
 import Feed from '../Feed';
 import { useFeedProvider } from '@/providers/FeedProvider';
-import Signin from './SignIn';
+import SignInButton from './SignInButton';
 
 const LandingPageContent = () => {
   const { feed } = useFeedProvider();
@@ -10,7 +10,7 @@ const LandingPageContent = () => {
   return (
     <div className="container flex flex-col items-center justify-center gap-4 px-4 text-center sm:gap-8 md:px-6">
       <div style={{ position: 'fixed', top: '12px', right: '12px' }}>
-        <Signin />
+        <SignInButton />
       </div>
       <LandingPageHeader />
       {feed?.length > 0 && <Feed feed={feed} />}
--- components/LandingPage/SignInButton.tsx
+++ components/LandingPage/SignInButton.tsx
@@ -1,23 +1,21 @@
-import { useNeynarProvider } from '@/providers/NeynarProvider';
 import { useCallback, useState } from 'react';
 
-const Signin = () => {
-  const { signer } = useNeynarProvider();
-  const [theme, setTheme] = useState('light');
-  const [variant, setVariant] = useState('neynar');
-  const [logoSize, setLogoSize] = useState('30px');
-  const [height, setHeight] = useState('48px');
-  const [width, setWidth] = useState('218px');
-  const [borderRadius, setBorderRadius] = useState('10px');
-  const [fontSize, setFontSize] = useState('16px');
-  const [fontWeight, setFontWeight] = useState('300');
-  const [padding, setPadding] = useState('8px 15px');
-  const [margin, setMargin] = useState('0px');
-  const [text, setText] = useState('hello');
-  const [color, setColor] = useState('');
-  const [backgroundColor, setBackgroundColor] = useState('');
-  const [styles, setStyles] = useState('');
-  const [customLogoUrl, setCustomLogoUrl] = useState('');
+const SignInButton = () => {
+  const theme = 'light';
+  const variant = 'neynar';
+  const logoSize = '30px';
+  const height = '48px';
+  const width = '218px';
+  const borderRadius = '10px';
+  const fontSize = '16px';
+  const fontWeight = '300';
+  const padding = '8px 15px';
+  const margin = '0px';
+  const text = 'hello';
+  const color = '';
+  const backgroundColor = '';
+  const styles = '';
+  const customLogoUrl = '';
 
   const client_id = process.env.NEXT_PUBLIC_NEYNAR_CLIENT_ID;
   const neynar_login_url =
@@ -72,4 +70,4 @@ const Signin = () => {
   return <div className="mx-5 flex flex-col items-center justify-center">{getButton()}</div>;
 };
 
-export default Signin;
+export default SignInButton;
