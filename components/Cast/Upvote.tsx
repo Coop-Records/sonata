a79b3decf3aa91f6425d3e0a7e6359c69f593e49
Sweets Sweetman <sweetmantech@gmail.com>
2024-04-15 13:31:21 -0300

    I fix dev logging.

--- components/Cast/Upvote.tsx
+++ components/Cast/Upvote.tsx
@@ -13,11 +13,7 @@ const Upvote = ({ target }: { target: Address }) => {
 
   const handleClick = async () => {
     const { signer_uuid } = signer;
-    console.log('SWEETS USE THIS', signer_uuid);
-    console.log('SWEETS LIKE THIS', target);
     const response = await createReaction(signer_uuid, target);
-    console.log('SWEETS RESPONSE', response);
-    // CHECK IF RESPONSE IS SUCCESSFUL
     await refetchData();
     if (response.success) {
       setUpvoted(true);
@@ -27,18 +23,10 @@ const Upvote = ({ target }: { target: Address }) => {
 
   const refetchData = async () => {
     const castResponse = await getCastReactions(target);
-    console.log('SWEETS castResponse', castResponse);
     setVotes(castResponse?.reactions?.length);
-    console.log('SWEETS SIGNER UPVOTE?', signer);
     const { fid } = signer;
-    console.log('SWEETS fid', fid);
-    console.log('SWEETS typeof fid', typeof fid);
-
-    // Iterate over all reactions to check if any reaction has the same fid as the signer
     const userHasUpvoted = castResponse.reactions.some((reaction: any) => reaction.user.fid == fid);
     if (userHasUpvoted) setUpvoted(true); // Update the upvoted state if the fid matches the signer's fid
-    // setUpvoted(userHasUpvoted); // Update the upvoted state based on the check
-    // console.log('SWEETS SIGNER UPVOTE?', signer, userHasUpvoted);
   };
 
   useEffect(() => {
--- hooks/useSigner.tsx
+++ hooks/useSigner.tsx
@@ -11,14 +11,12 @@ declare global {
 
 const useSigner = () => {
   const [signer, setSigner] = useState<Signer | null>(() => {
-    // Retrieve the signer from localStorage on initial load
     const savedSigner = localStorage.getItem('signer');
     return savedSigner ? JSON.parse(savedSigner) : null;
   });
 
   useEffect(() => {
     window.onSignInSuccess = (data) => {
-      console.log('SWEETS HANDLE SUCCESS', data);
       const signerData = data as Signer;
       setSigner(signerData);
       localStorage.setItem('signer', JSON.stringify(signerData));
@@ -32,16 +30,12 @@ const useSigner = () => {
   }, []);
 
   useEffect(() => {
-    // Identify or create the script element
     let script = document.getElementById('siwn-script') as HTMLScriptElement | null;
-
     if (!script) {
       script = document.createElement('script');
       script.id = 'siwn-script';
       document.body.appendChild(script);
     }
-
-    // Set attributes and source of the script
     script.src = 'https://neynarxyz.github.io/siwn/raw/1.2.0/index.js';
     script.async = true;
     script.defer = true;
@@ -49,12 +43,9 @@ const useSigner = () => {
     document.body.appendChild(script);
 
     return () => {
-      // Remove the script from the body
       if (script) {
         document.body.removeChild(script);
       }
-
-      // Remove the button if it exists
       let button = document.getElementById('siwn-button');
       if (button && button.parentElement) {
         button.parentElement.removeChild(button);
