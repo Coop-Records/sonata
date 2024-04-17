667e7356bf67037161a5c000b2cf12aff3bddd4f
Arpit Gupta <arpitgupta1214@gmail.com>
2024-04-18 02:16:40 +0530

    trending tab added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,24 @@
-'use client';
-
-import { AuthKitProvider } from '@farcaster/auth-kit';
+import Providers from '@/providers';
 import '../styles/globals.css';
 import '@farcaster/auth-kit/styles.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Header from '@/components/Header';
 
 export const viewport = {
   width: 'device-width',
   initialScale: 1.0,
 };
 
-const config = {
-  relay: 'https://relay.farcaster.xyz',
-  rpcUrl: 'https://mainnet.optimism.io',
-  domain: 'sonatamusic.vercel.app',
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <AuthKitProvider config={config}>
+      <body className="font-helvetica">
+        <Providers>
+          <Header />
           {children}
           <ToastContainer />
-        </AuthKitProvider>
+        </Providers>
       </body>
     </html>
   );
---
+++ app/trending/page.tsx
@@ -0,0 +1,7 @@
+export default function Trending() {
+  return (
+    <div className="container pt-10">
+      <h1 className="text-center text-3xl">Trending</h1>
+    </div>
+  );
+}
