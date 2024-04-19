abfa21272b4a625b8807b8257393d87863e4d47f
Arpit Gupta <arpitgupta1214@gmail.com>
2024-04-20 01:23:14 +0530

    Soundcloud custom embed added custom player added

--- components/Cast/SoundCloudEmbed.tsx
+++ components/Cast/SoundCloudEmbed.tsx
@@ -1,48 +1,80 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import SC from '@/lib/soundcloud/mediaApi';
+import CustomEmbed from './CustomEmbed';
 
 const SoundCloudEmbed = ({ trackUrl }: any) => {
-  const [embedHtml, setEmbedHtml] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [playing, setPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [position, setPosition] = useState(0);
   const [embedData, setEmbedData] = useState({} as any);
+  const [widget, setWidget] = useState({} as any);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
     const init = async () => {
       const oEmbedUrl = `https://soundcloud.com/oembed?format=json&url=${encodeURIComponent(trackUrl)}`;
       const response = await fetch(oEmbedUrl);
       const data = await response.json();
-      setEmbedHtml(data.html);
-      setEmbedData(data);
+      const srcRegex = /src="([^"]+)"/;
+      const match = data.html.match(srcRegex);
+      const src = match ? match[1] : null;
+      setEmbedData({ ...data, src });
+      setLoading(false);
     };
     init();
   }, [trackUrl]);
 
+  useEffect(() => {
+    const iframe = iframeRef.current;
+    if (!(iframe && embedData.src)) return;
+    const widget = SC.Widget(iframe);
+
+    widget.bind(SC.Widget.Events.READY, function () {
+      widget.bind(SC.Widget.Events.PLAY, () => {
+        setPlaying(true);
+      });
+
+      widget.bind(SC.Widget.Events.PAUSE, () => {
+        setPlaying(false);
+      });
+
+      widget.bind(SC.Widget.Events.PLAY_PROGRESS, (position: any) => {
+        setPosition(position.currentPosition);
+      });
+
+      widget.getDuration((duration: number) => {
+        setDuration(duration);
+      });
+    });
+
+    setWidget(widget);
+  }, [iframeRef.current, embedData.src]);
+
+  if (loading) return <></>;
+
   return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: embedData?.html }} />
-      <div
-        style={{
-          fontSize: '10px',
-          color: '#cccccc',
-          lineBreak: 'anywhere',
-          wordBreak: 'normal',
-          overflow: 'visible',
-          textOverflow: 'clip',
-          fontFamily:
-            'Interstate, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Garuda, Verdana, Tahoma, sans-serif',
-          fontWeight: '100',
-        }}
-        className="flex justify-center"
-      >
-        <a
-          href={embedData?.author_url}
-          title={embedData?.author_name}
-          target="_blank"
-          style={{ color: '#cccccc', textDecoration: 'none' }}
-        >
-          {embedData?.author_name}·{' '}
-          <p title={embedData?.title} style={{ color: '#cccccc', textDecoration: 'none' }}>
-            {embedData?.title}
-          </p>
-        </a>
+    <div className="w-full z-0 relative">
+      <CustomEmbed
+        artistName={embedData.author_name}
+        trackName={embedData.title.split(' - ')[0].split(' by ')[0]}
+        artworkUrl={embedData.thumbnail_url}
+        onPause={() => widget.pause()}
+        onPlay={() => widget.play()}
+        playing={playing}
+        duration={duration}
+        position={position}
+      />
+      <div className="absolute top-0 left-0 -z-10 opacity-0">
+        <iframe
+          width="100%"
+          height="166"
+          scrolling="no"
+          frameBorder="no"
+          allow="autoplay"
+          src={embedData.src}
+          ref={iframeRef}
+        />
       </div>
     </div>
   );
--- lib/utils.tsx
+++ lib/utils.tsx
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDuration(duration: number) {
+  const durationSeconds = Math.floor(duration / 1000);
+  const minutes = Math.floor(durationSeconds / 60);
+  const seconds = Math.floor(durationSeconds % 60);
+  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
