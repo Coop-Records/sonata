22aed0afb6bd1ca1a48b9da9ea5d113f739cbfe3
Arpit Gupta <arpitgupta1214@gmail.com>
2024-04-23 11:34:32 +0530

    platform filter added

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
 import Cast from '../Cast';
 import { Cast as CastType } from '@/types/Cast';
+import Filter from './Filter';
+import { useMemo, useState } from 'react';
+import findValidEmbed from '@/lib/findValidEmbed';
+import { FeedFilter } from '@/types/FeedFilter';
 
 export default function Feed({ feed }: any) {
+  const [filter, setFilter] = useState<FeedFilter>({});
+
+  const handleFilterChange = (change: any) => {
+    console.log({ change });
+    setFilter((prev) => ({ ...prev, ...change }));
+  };
+
+  if (!(feed && feed.length)) return <></>;
+
+  const filteredFeed: any = useMemo(
+    () => feed.filter((cast: CastType) => findValidEmbed(cast, filter)),
+    [filter.platform],
+  );
+
   return (
-    <div className="max-w-[333px] md:max-w-[444px]">
-      {feed && feed.length && feed.map((cast: CastType) => <Cast key={cast.hash} cast={cast} />)}
+    <div className="flex items-start gap-10">
+      <div className="max-w-[333px] md:max-w-[444px]">
+        {filteredFeed.map((cast: CastType) => (
+          <Cast key={cast.hash} cast={cast} />
+        ))}
+      </div>
+      <Filter onChange={handleFilterChange} value={filter} />
     </div>
   );
 }
---
+++ types/FeedFilter.d.ts
@@ -0,0 +1,3 @@
+export type FeedFilter = {
+  platform?: string;
+};
