333b765b81cf3c9842e210af842f69efbaae599e
Arpit Gupta <arpitgupta1214@gmail.com>
2024-04-25 06:37:30 +0530

    Signin button updated update logo to farcaster logo update text to Sign In on signed in, change button to say logout

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -1,72 +1,24 @@
-import { useCallback } from 'react';
+import { useNeynarProvider } from '@/providers/NeynarProvider';
+import Image from 'next/image';
 
 const SignInButton = () => {
-  const theme = 'light';
-  const variant = 'neynar';
-  const logoSize = '30px';
-  const height = '48px';
-  const width = '218px';
-  const borderRadius = '10px';
-  const fontSize = '16px';
-  const fontWeight = '300';
-  const padding = '8px 15px';
-  const margin = '0px';
-  const text = 'hello';
-  const color = '';
-  const backgroundColor = '';
-  const styles = '';
-  const customLogoUrl = '';
+  const { signIn, signOut, user } = useNeynarProvider();
 
-  const client_id = process.env.NEXT_PUBLIC_NEYNAR_CLIENT_ID;
-  const neynar_login_url = 'https://app.neynar.com/login';
-
-  if (!client_id) {
-    throw new Error('NEXT_PUBLIC_NEYNAR_CLIENT_ID is not defined in .env');
-  }
-
-  const getButton = useCallback(() => {
-    return (
-      <div
-        className="neynar_signin"
-        data-client_id={client_id}
-        data-neynar_login_url={neynar_login_url}
-        data-success-callback="onSignInSuccess"
-        data-theme="light"
-        data-variant="neynar"
-        data-logo_size={logoSize}
-        data-height={height}
-        data-width={width}
-        data-border_radius={borderRadius}
-        data-font_size={fontSize}
-        data-font_weight={fontWeight}
-        data-padding={padding}
-        data-margin={margin}
-        data-text={text}
-        data-color={color}
-        data-background_color={backgroundColor}
-        data-styles={styles}
-        data-custom_logo_url={customLogoUrl}
-      ></div>
-    );
-  }, [
-    theme,
-    variant,
-    logoSize,
-    height,
-    width,
-    borderRadius,
-    fontSize,
-    fontWeight,
-    padding,
-    margin,
-    text,
-    color,
-    backgroundColor,
-    styles,
-    customLogoUrl,
-  ]);
-
-  return getButton();
+  return (
+    <button
+      onClick={user ? signOut : signIn}
+      className="rounded-md bg-black text-white py-2 px-4 flex gap-2 items-center font-light"
+    >
+      {user ? (
+        'Logout'
+      ) : (
+        <>
+          <Image src="/images/warpcast.png" width={32} height={32} alt="" />
+          Sign In
+        </>
+      )}
+    </button>
+  );
 };
 
 export default SignInButton;
