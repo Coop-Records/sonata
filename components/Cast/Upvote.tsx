ffbfefaf3548657eecd1337e5db7f4eda7389467
Arpit Gupta <arpitgupta1214@gmail.com>
2024-04-30 14:59:01 +0530

    added sign in dialog

--- components/Cast/Upvote.tsx
+++ components/Cast/Upvote.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
 import { cn } from '@/lib/utils';
 import { useToast } from '../ui/use-toast';
+import SignInDialog from '../SignInDialog';
 
 const Upvote = ({ cast }: { cast: Cast }) => {
   const { toast } = useToast();
@@ -13,9 +14,12 @@ const Upvote = ({ cast }: { cast: Cast }) => {
     Boolean(signer && cast.reactions.likes.some((like) => like?.fid === String(signer?.fid))),
   );
   const [votes, setVotes] = useState<number>(cast.reactions.likes.length);
-
+  const [signInModalOpen, setSignInModalOpen] = useState<boolean>(false);
   const handleClick = async () => {
-    if (!signer) return;
+    if (!signer) {
+      setSignInModalOpen(true);
+      return;
+    }
 
     const { signer_uuid } = signer;
     const response = await createReaction(signer_uuid, cast.hash);
@@ -30,12 +34,15 @@ const Upvote = ({ cast }: { cast: Cast }) => {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <button type="button" onClick={handleClick}>
-        <FaArrowUp className={cn('text-2xl text-gray-400', upvoted && 'text-black')} />
-      </button>
-      <span className="font-inter text-xl font-semibold">{votes}</span>
-    </div>
+    <>
+      <div className="flex flex-col items-center">
+        <button type="button" onClick={handleClick}>
+          <FaArrowUp className={cn('text-2xl text-gray-400', upvoted && 'text-black')} />
+        </button>
+        <span className="font-inter text-xl font-semibold">{votes}</span>
+      </div>
+      <SignInDialog open={signInModalOpen} setOpen={setSignInModalOpen} />
+    </>
   );
 };
 
