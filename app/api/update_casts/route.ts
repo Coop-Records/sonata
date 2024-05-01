67f81923b4b09d2e61173ef0880ab89cb508db7e
Sweets Sweetman <sweetmantech@gmail.com>
2024-05-01 16:05:04 -0700

    I init update_casts.

---
+++ app/api/update_casts/route.ts
@@ -0,0 +1,99 @@
+import getFeedFromTime from '@/lib/neynar/getFeedFromTime';
+import { createClient } from '@supabase/supabase-js';
+import { isEmpty } from 'lodash';
+import { NextResponse } from 'next/server';
+
+const SUPABASE_URL = process.env.SUPABASE_URL as string;
+const SUPABASE_KEY = process.env.SUPABASE_KEY as string;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const processEntriesInBatches = async (entries: any[], batchSize = 50) => {
+  for (let i = 0; i < entries.length; i += batchSize) {
+    const batch = entries.slice(i, i + batchSize);
+    await Promise.all(batch.map((entry: any) => processSingleEntry(entry)));
+  }
+};
+
+const processSingleEntry = async (cast: {
+  reactions: { likes: string | any[] };
+  author: { verifications: any; fid: any };
+  timestamp: any;
+}) => {
+  const likes = cast.reactions?.likes?.length ?? 0;
+  const address = cast?.author?.verifications ? cast?.author?.verifications : undefined;
+  const timestamp = cast?.timestamp;
+  const fid = cast?.author?.fid;
+
+  if (!isEmpty(address)) {
+    await callUpdateTips(address, fid, likes, 1, timestamp);
+  }
+};
+
+const getResponse = async (): Promise<NextResponse> => {
+  console.log("SWEETS UPDATE CAST IN SUPABASE")
+  const { data: tip_query_date } = await supabase
+    .from('tip_query_date')
+    .select('last_checked')
+    .eq('id', 1)
+    .single();
+  const lastChecked = tip_query_date ? new Date(tip_query_date.last_checked) : new Date();
+
+  const [spotify, soundCloud, soundxyz] = await Promise.all([
+    getFeedFromTime('spotify.com/track', lastChecked),
+    getFeedFromTime('soundcloud.com', lastChecked),
+    getFeedFromTime('sound.xyz', lastChecked),
+  ]);
+
+  const allEntries: any[] = [];
+
+  allEntries.push(...spotify, ...soundCloud, ...soundxyz);
+
+  await processEntriesInBatches(allEntries);
+
+  const newLastChecked = allEntries.reduce((max, cast) => {
+    const current = new Date(cast.timestamp);
+    return current > max ? current : max;
+  }, lastChecked);
+
+  await supabase.rpc('update_daily_tip_allocation');
+
+  await supabase.from('tip_query_date').upsert({ id: 1, last_checked: newLastChecked });
+
+  return NextResponse.json({ message: 'success' }, { status: 200 });
+};
+
+async function callUpdateTips(
+  walletAddress: string,
+  fid: string,
+  totalLikes: number,
+  numPosts: number,
+  firstPostDate: string,
+) {
+  const firstPostDateISO = new Date(firstPostDate).toISOString();
+
+  const { data, error } = await supabase.rpc('update_tips', {
+    p_wallet_address: walletAddress,
+    p_fid: fid,
+    p_total_likes: totalLikes,
+    p_num_posts: numPosts,
+    p_first_post_date: firstPostDateISO,
+  });
+
+  if (error) {
+    console.error('Error calling function:', error);
+    return null;
+  }
+
+  console.log('Function called successfully, result:', data);
+  return data;
+}
+
+export async function GET(): Promise<Response> {
+  await getResponse().catch((error) => {
+    console.error('Error in background task:', error);
+  });
+  return NextResponse.json({ message: 'success' }, { status: 200 });
+}
+
+export const dynamic = 'force-dynamic';
--- components/Header/Title.tsx
+++ components/Header/Title.tsx
@@ -5,7 +5,7 @@ export default function Title() {
   return (
     <div className="flex items-center justify-center space-x-2 md:space-x-3">
       <div>
-        <Image src="/images/logo.png" width={33} height={33} alt="logo" />
+        <Image src="/images/logo2.png" width={33} height={33} alt="logo" />
       </div>
       <div className="text-4xl font-bold">{TITLE}</div>
     </div>
