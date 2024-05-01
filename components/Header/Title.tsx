fdff6b8b32914f53840ae29a9d70f6c10232571c
Sweets Sweetman <sweetmantech@gmail.com>
2024-05-01 15:54:30 -0700

    I update logo.

--- components/Header/Title.tsx
+++ components/Header/Title.tsx
@@ -1,9 +1,13 @@
 import { TITLE } from '@/lib/consts';
+import Image from 'next/image';
 
 export default function Title() {
   return (
-    <div className="space-y-2 text-center md:space-y-3">
-      <p className="text-4xl font-bold tracking-tighter sm:text-5xl">{TITLE}</p>
+    <div className="flex items-center justify-center space-x-2 md:space-x-3">
+      <div>
+        <Image src="/images/logo.png" width={33} height={33} alt="logo" />
+      </div>
+      <div className="text-4xl font-bold">{TITLE}</div>
     </div>
   );
 }
