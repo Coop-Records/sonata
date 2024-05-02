0e810a72150ad7663fde7d7fb156cdb2bc4fb4eb
Sweets Sweetman <sweetmantech@gmail.com>
2024-05-01 22:09:02 -0700

    I update to use existing hook.

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -1,20 +1,18 @@
 'use client';
+
 import { Cast as CastType } from '@/types/Cast';
 import { useEffect, useMemo } from 'react';
 import { useFeedProvider } from '@/providers/FeedProvider';
 import Cast from '@/components/Cast';
 import filterFeed from '@/lib/filterFeed';
+import useNewCasts from '@/hooks/useNewCasts';
 
 export default function Feed({ feed }: { feed: CastType[] }) {
   const { filter, setFeed } = useFeedProvider();
+  useNewCasts();
 
   useEffect(() => {
-    const init = async () => {
-      await fetch('/api/getNewCasts');
-    };
-
     setFeed(feed);
-    init();
   }, [feed, setFeed]);
 
   const filteredFeed: any = useMemo(() => filterFeed(feed, filter), [filter, feed]);
