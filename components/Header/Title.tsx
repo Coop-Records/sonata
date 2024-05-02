f3449a420d7a64fe654278a811afa71856a30350
Sweets Sweetman <sweetmantech@gmail.com>
2024-05-01 22:06:55 -0700

    I update export structure.

--- components/Header/Title.tsx
+++ components/Header/Title.tsx
@@ -1,9 +1,9 @@
 import { TITLE } from '@/lib/consts';
 
-export default function Title() {
-  return (
-    <div className="space-y-2 text-center md:space-y-3">
-      <p className="text-4xl font-bold tracking-tighter sm:text-5xl">{TITLE}</p>
-    </div>
-  );
-}
+const Title = () => (
+  <div className="space-y-2 text-center md:space-y-3">
+    <p className="text-4xl font-bold tracking-tighter sm:text-5xl">{TITLE}</p>
+  </div>
+);
+
+export default Title;
