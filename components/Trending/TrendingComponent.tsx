8a1e06c6a1ab2432b631877decd0e0bcd202c5a2
ramiechaarani <ramie.chaarani@gmail.com>
2024-05-03 15:25:42 -0700

    Add in fetchPoints

---
+++ components/Trending/TrendingComponent.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import useFeed from "@/hooks/useFeed";
+import Feed from "../Feed";
+
+export default function TrendingComponent() {
+  const { feed } = useFeed();
+
+  feed.sort((cast1: any, cast2: any) => {
+    return cast2.reactions.likes.length - cast1.reactions.likes.length;
+  });
+
+  return feed?.length > 0 ? <Feed feed={feed} /> : <></>;
+  }
+  
---
+++ hooks/useFeed.tsx
@@ -0,0 +1,75 @@
+import getSortedFeeds from '@/lib/neynar/getCombinedFeeds';
+import { useEffect, useState } from 'react';
+import getFeed from '@/lib/neynar/getFeed';
+import { useSupabaseProvider } from '@/providers/SupabaseProvider';
+import _ from 'lodash';
+
+const useFeed = () => {
+  const [feed, setFeed] = useState<any[]>([]);
+  const { supabaseClient } = useSupabaseProvider();
+
+  const fetchAndUpdatePoints = async (postHash: string) => {
+    const { data, error } = await supabaseClient
+      .from('posts')
+      .select('points,degen')
+      .eq('post_hash', postHash)
+      .single();
+
+    if (error) {
+      console.error('Error fetching points for post:', error);
+      return;
+    }
+
+    if (data) {
+      setFeed((currentFeed) =>
+        currentFeed.map((post) => {
+          if (post.hash === postHash) {
+            return { ...post, points: data.points, degen: data.degen };
+          }
+          return post;
+        }),
+      );
+    }
+  };
+
+  const fetchPoints = async (postHashes: string[]) => {
+    const { data, error } = await supabaseClient
+      .from('posts')
+      .select('post_hash, points, degen')
+      .in('post_hash', postHashes);
+
+    if (error) {
+      console.error('Error fetching points:', error);
+      return {};
+    }
+    return data.reduce(
+      (
+        acc: { [x: string]: any },
+        item: { post_hash: string | number; points: any; degen: any },
+      ) => {
+        acc[item.post_hash] = { points: item.points, degen: item.degen }; // Store both points and degen
+        return acc;
+      },
+      {},
+    );
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      const sortedFeeds = await getSortedFeeds();
+      const postHashes = sortedFeeds.map((post) => post.hash);
+      const pointsMap = await fetchPoints(postHashes);
+      const feedsWithPoints = sortedFeeds.map((post) => ({
+        ...post,
+        points: pointsMap[post.hash]?.points || 0, // Safe access to points
+        degen: pointsMap[post.hash]?.degen || 0, // Safe access to degen
+      }));
+      setFeed(feedsWithPoints);
+    };
+    init();
+  }, []);
+
+  return { feed, fetchAndUpdatePoints };
+};
+
+export default useFeed;
