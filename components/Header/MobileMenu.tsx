81bb2ce8916de99e9a7e9a0537c2c9fd7b2ac9a6
Sweets Sweetman <sweetmantech@gmail.com>
2024-05-04 00:55:57 -0700

    I fix build errors.

--- components/Header/MobileMenu.tsx
+++ components/Header/MobileMenu.tsx
@@ -1,5 +1,5 @@
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet';
-import PlatformFilter from '../Feed/PlatformFilter';
+// import PlatformFilter from '../Feed/PlatformFilter';
 import BalanceInfo from './BalanceInfo';
 import SignInButton from '../SignInButton';
 import { useNeynarProvider } from '@/providers/NeynarProvider';
