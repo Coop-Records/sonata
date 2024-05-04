83d76494dfc4989f00c59aebbed3a0c051714a76
Sweets Sweetman <sweetmantech@gmail.com>
2024-05-04 15:19:13 -0700

    I prevent points for self-upvote

--- components/Cast/Upvote.tsx
+++ components/Cast/Upvote.tsx
@@ -7,13 +7,18 @@ import { cn } from '@/lib/utils';
 import SignInDialog from '../SignInDialog';
 import getCastLikes from '@/lib/neynar/getCastLikes';
 import { SupabasePost } from '@/types/SupabasePost';
+import useSignInModal from '@/hooks/useSignInModal';
+import { Signer } from '@neynar/nodejs-sdk/build/neynar-api/v2';
 
 const Upvote = ({ cast }: { cast: SupabasePost }) => {
-  const { signer } = useNeynarProvider();
+  const { signer, user } = useNeynarProvider();
   const [upvoted, setUpvoted] = useState(false);
   const [votes, setVotes] = useState<number>(cast.likes || 0);
-  const [signInModalOpen, setSignInModalOpen] = useState<boolean>(false);
+  const { isOpen, setIsOpen, checkLoggedIn } = useSignInModal();
   const { tip } = useTipProvider();
+  const userFid = user?.fid;
+  const castAuthorFid = cast.author?.fid;
+  const isSelfPost = userFid === castAuthorFid;
 
   useEffect(() => {
     const updateReaction = async () => {
@@ -30,22 +35,16 @@ const Upvote = ({ cast }: { cast: SupabasePost }) => {
     }
   }, [cast.post_hash, signer?.fid]);
 
-  useEffect(() => {
-    if (signer) setSignInModalOpen(false);
-  }, [signer]);
-
   const handleClick = async () => {
-    if (!signer) {
-      setSignInModalOpen(true);
-      return;
-    }
+    if (!checkLoggedIn()) return;
 
-    const { signer_uuid } = signer;
+    const { signer_uuid } = signer as Signer;
     const response = await createReaction(signer_uuid, cast.post_hash);
 
     if (response.success) {
       setUpvoted(true);
       setVotes(votes + 1);
+      if (isSelfPost) return;
       await tip(10, cast.post_hash, cast.author.verifications);
     }
   };
@@ -58,7 +57,7 @@ const Upvote = ({ cast }: { cast: SupabasePost }) => {
         </button>
         <span className="font-inter text-xl font-semibold">{votes}</span>
       </div>
-      <SignInDialog open={signInModalOpen} setOpen={setSignInModalOpen} />
+      <SignInDialog open={isOpen} setOpen={setIsOpen} />
     </>
   );
 };
