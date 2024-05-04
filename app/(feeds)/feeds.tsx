940e943f7f0b5f2ab6075d0934121ce0019085aa
SeniorDev <tech.eng.me@gmail.com>
2024-05-03 22:13:52 -0400

    Revert "I fix spotify not playing bug"
    This reverts commit 9076325fd893bead083436d3ff3f99477df42b42.

--- app/(feeds)/feeds.tsx
+++ app/(feeds)/feeds.tsx
@@ -1,36 +1,21 @@
 'use client';
 
-import Feed from '@/components/Feed';
-import useFeed from '@/hooks/useFeed';
+import RecentComponent from '@/components/Recent/RecentComponent';
+import TrendingComponent from '@/components/Trending/TrendingComponent';
 import { FeedType, useFeedProvider } from '@/providers/FeedProvider';
-import { useMemo } from 'react';
 
 const Feeds = () => {
-  const { feed } = useFeed();
   const { feedType } = useFeedProvider();
   const isTrending = feedType === FeedType.Trending;
   const isRecent = feedType === FeedType.Recent;
 
-  const recentFeeds = useMemo(
-    () =>
-      [...feed].sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()),
-    [feed],
-  );
-  const trendingFeeds = useMemo(
-    () =>
-      [...feed].sort((cast1: any, cast2: any) => {
-        return cast2.reactions.likes.length - cast1.reactions.likes.length;
-      }),
-    [feed],
-  );
-
   return (
     <>
       <div className={`${isRecent ? 'hidden' : ''}`} key="trending">
-        {trendingFeeds?.length ? <Feed feed={trendingFeeds} /> : <></>}
+        <TrendingComponent />
       </div>
       <div className={`${isTrending ? 'hidden' : ''}`} key="recent">
-        {recentFeeds?.length ? <Feed feed={recentFeeds} /> : <></>}
+        <RecentComponent />
       </div>
     </>
   );
