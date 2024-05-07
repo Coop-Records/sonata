073aa414a348be548ce5e83743d72911da11a0dd
SeniorDev <tech.eng.me@gmail.com>
2024-05-07 16:05:01 -0400

    Reapply "Reapply "I fix slow loading issue""
    This reverts commit 85d34826d10c2e09aa56fb1b0213f1af32ef4b97.

--- hooks/useFeed.tsx
+++ hooks/useFeed.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useSupabaseProvider } from '@/providers/SupabaseProvider';
-import { useFeedProvider } from '@/providers/FeedProvider';
 import fetchPosts from '@/lib/fetchPosts';
 import getSortedFeed from '@/lib/getSortedFeed';
 import mergeArraysUniqueByPostHash from '@/lib/mergeArraysUniqueByPostHash';
@@ -8,15 +7,11 @@ import mergeArraysUniqueByPostHash from '@/lib/mergeArraysUniqueByPostHash';
 const useFeed = ({ feedType }: { feedType: string }) => {
   const [feed, setFeed] = useState<any[]>([]);
   const { supabaseClient } = useSupabaseProvider();
-  const { filter } = useFeedProvider();
 
   const getFeed = async (start: number) => {
-    const { posts } = await fetchPosts(supabaseClient, filter, feedType, start);
+    const { posts } = await fetchPosts(supabaseClient, feedType, start);
     setFeed((prev) => {
-      const filteredPrev = filter.channel
-        ? prev.filter((item) => item.channelId === filter.channel)
-        : prev;
-      const mergedUnique = mergeArraysUniqueByPostHash(filteredPrev, posts);
+      const mergedUnique = mergeArraysUniqueByPostHash(prev, posts);
       const sortedFeed = getSortedFeed(mergedUnique, feedType);
       return sortedFeed;
     });
@@ -30,12 +25,8 @@ const useFeed = ({ feedType }: { feedType: string }) => {
   }, []);
 
   useEffect(() => {
-    if (filter.channel) {
-      getFeed(0);
-    } else {
-      getFeed(feed.length);
-    }
-  }, [filter.channel]);
+    getFeed(0);
+  }, []);
 
   return { feed, getFeed };
 };
