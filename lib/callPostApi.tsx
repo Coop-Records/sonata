1d1c4acbbb8a17fe7593f2556b0b80879a4b5cdc
ramiechaarani <ramie.chaarani@gmail.com>
2024-05-08 08:50:14 -0700

    Update callPostApi.tsx

--- lib/callPostApi.tsx
+++ lib/callPostApi.tsx
@@ -15,7 +15,7 @@ const callPostApi = async (signer_uuid: string | undefined, url: string): Promis
       },
       body: stringifiedBody,
     });
-    const getNewCastsResponse = await fetch('/api/jobs/getNewCasts');
+    await fetch('/api/jobs/getNewCasts');
     const data = await res.json();
     return data;
   } catch (error) {
