7dfc73575ac062b6d7d5af539fe0cb31a0d1c16b
Sweets Sweetman <sweetmantech@gmail.com>
2024-05-11 15:11:45 -0500

    I rollback unused code.

--- hooks/useSpotifyIframe.tsx
+++ hooks/useSpotifyIframe.tsx
@@ -13,30 +13,19 @@ const useSpotifyIframe = (trackUrl: string) => {
 
   useEffect(() => {
     const init = async () => {
-      console.log('SWEETS SONG LINK for trackUrl', trackUrl);
       const response = await fetch(`/api/songLink/fetchSoundcloudUrl?trackUrl=${trackUrl}`);
       const data = await response.json();
       const responseUrl = data?.linksByPlatform?.soundcloud?.url;
-      console.log('SWEETS SONG LINK', responseUrl);
       setSoundcloudUrl(responseUrl);
     };
     init();
   }, [trackUrl]);
 
-  const togglePlay = () => {
-    console.log('SWEETS PLAY WITH CONTROLLER', embedController);
-    embedController.play();
-  };
-
   const pauseMusic = () => {
-    console.log('SWEETS PAUSE WITH CONTROLLER', embedController);
-
     embedController.pause();
   };
 
   const playMusic = () => {
-    console.log('SWEETS PLAY WITH CONTROLLER', embedController);
-
     embedController.play();
   };
 
@@ -65,66 +54,22 @@ const useSpotifyIframe = (trackUrl: string) => {
         event.source === iframeRef.current.contentWindow
       ) {
         const { type } = event.data;
-        console.log('Sweets event.data', event.data);
-
         if (type === 'ready') {
           setIsReady(true);
-          console.log('Sweets isREADY!!!+++!!!');
-
           return;
         }
         if (type === 'playback_update') {
           const { isPaused: response } = event.data.payload;
-          console.log('Sweets isPaused?', response);
           setIsPlaying(!response);
         }
       }
     };
-
-    console.log('SWEETS ADDED LISTENER FOR MESSAGES FROM IFRAME');
     window.addEventListener('message', handleMessage);
 
     return () => {
       window.removeEventListener('message', handleMessage);
     };
   }, [iframeRef]);
-
-  useEffect(() => {
-    // Function to initialize the API
-    const initializeSpotifyAPI = () => {
-      (window as any).onSpotifyIframeApiReady = (IFrameAPI: any) => {
-        if (iframeRef.current) {
-          const options = { uri: 'spotify:track:7fzHQizxTqy8wTXwlrgPQQ' };
-          const callback = (EmbedController: any) => {
-            console.log('SWEETS EmbedController', EmbedController);
-            setEmbedController(EmbedController);
-          };
-          IFrameAPI.createController(iframeRef.current, options, callback);
-        }
-      };
-    };
-
-    // Check if the Spotify script is already loaded
-    const existingScript = document.getElementById('spotify-iframe-api');
-    if (!existingScript) {
-      const script = document.createElement('script');
-      script.id = 'spotify-iframe-api';
-      script.src = 'https://sdk.scdn.co/spotify-player.js';
-      script.onload = initializeSpotifyAPI; // Initialize API once the script is loaded
-      document.body.appendChild(script);
-    } else {
-      // Initialize immediately if the script is already there
-      initializeSpotifyAPI();
-    }
-
-    return () => {
-      // Optional cleanup if needed
-      const scriptToRemove = document.getElementById('spotify-iframe-api');
-      if (scriptToRemove) {
-        document.body.removeChild(scriptToRemove);
-      }
-    };
-  }, []);
 
   return {
     embedController,
