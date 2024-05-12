ceaedd613f773b6e0d8b8dcaa0c83cb4a06e3800
Arpit Gupta <arpitgupta1214@gmail.com>
2024-05-12 07:50:01 +0530

    build fix

--- providers/SoundContextProvider.tsx
+++ providers/SoundContextProvider.tsx
@@ -1,10 +1,15 @@
 import { PlayerAction } from './PlayerProvider';
-import { Dispatch, useContext, createContext, useEffect, useMemo } from 'react';
+import { Dispatch, useContext, createContext, useEffect, useState } from 'react';
 
 const soundContext = createContext<any>(null);
 
 export function SoundContextProvider({ children }: { children: React.ReactNode }) {
-  const audio = useMemo(() => new Audio(), []);
+  const [audio, setAudio] = useState<HTMLAudioElement>();
+
+  useEffect(() => {
+    setAudio(new Audio());
+  }, []);
+
   return <soundContext.Provider value={{ audio }}>{children}</soundContext.Provider>;
 }
 
@@ -12,6 +17,7 @@ export const useSoundContext = (dispatch: Dispatch<PlayerAction>) => {
   const { audio }: any = useContext(soundContext);
 
   useEffect(() => {
+    if (!audio) return;
     audio.ontimeupdate = () => {
       dispatch({ type: 'PROGRESS', payload: { position: audio.currentTime * 1000 } });
     };
