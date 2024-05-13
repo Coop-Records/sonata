04113a6b2f75cbfc69f7bf8bef22c3d2cf8fc0e1
SeniorDev <tech.eng.me@gmail.com>
2024-05-13 02:06:55 -0400

    I fix conflict

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -1,30 +1,12 @@
 import Cast from '@/components/Cast';
-import { useEffect } from 'react';
-import { FeedType, useFeedProvider } from '@/providers/FeedProvider';
 import { SupabasePost } from '@/types/SupabasePost';
-import useFeed from '@/hooks/useFeed';
-import Loader from '../Loader';
 
-export default function Feed() {
-  const { setFeed } = useFeedProvider();
-  const { feed, hasNextPage, observerElem } = useFeed({
-    feedType: FeedType.Trending,
-  });
-
-  useEffect(() => {
-    setFeed(feed);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [feed]);
-
-  if (feed.length === 0) return <></>;
-
+export default function Feed({ feed }: { feed: SupabasePost[] }) {
   return (
     <div className="max-w-full grow space-y-6">
       {feed.map((cast: SupabasePost) => (
         <Cast key={cast.post_hash} cast={cast} />
       ))}
-      <div ref={observerElem} className="w-full h-4" />
-      {hasNextPage && <Loader />}
     </div>
   );
 }
