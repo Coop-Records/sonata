a5a29e225c3dff190281b26ac42ee65343675b1c
ramiechaarani <ramie.chaarani@gmail.com>
2024-05-17 15:57:00 -0700

    Update layout.tsx

--- app/cast/[username]/[hash]/layout.tsx
+++ app/cast/[username]/[hash]/layout.tsx
@@ -7,13 +7,13 @@ export default function FeedLayout({ children }: { children: ReactNode }) {
   return (
     <main>
       <meta property="of:accepts:xmtp" content="2024-02-01" />
-      <div className="container flex justify-center font-helvetica bg-blend-color-burn md:gap-6">
+      <div className="container flex justify-center font-helvetica bg-blend-color-burn md:gap-6 grow">
         <div className="min-w-48 pt-16 max-md:hidden" />
 
         <Separator orientation="vertical" className="max-md:hidden" />
 
         <div className="flex max-w-full grow flex-col items-center gap-4">
-          <div className="w-full grow overflow-hidden">{children}</div>
+          <div className="w-full grow">{children}</div>
         </div>
         <Separator orientation="vertical" className="max-md:hidden" />
         <div className="min-w-48 pt-16 max-md:hidden" />
