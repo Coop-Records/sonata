c3f3611499da50a8f5add071dd9063ab5d3471ac
TahirHameed74 <tsb_125@yahoo.com>
2024-06-03 20:44:39 +0500

    add airdrop claim button

--- components/ClaimAirdropButton/ClaimAirdropButton.tsx
+++ components/ClaimAirdropButton/ClaimAirdropButton.tsx
@@ -17,9 +17,15 @@ export default function ClaimAirdropButton() {
 
   return (
     <div>
-      <Button type="button" className="space-x-2" onClick={handleClick}>
-        <span>Claim</span>
+      <Button
+        type="button"
+        className="flex items-center space-x-2 rounded-full py-6 px-6"
+        onClick={handleClick}
+      >
+        <img width={24} height={24} className="fill-white" src="images/notes.png" />
+        <span className="text-[14px] line-height-[16.8px]">Claim Airdrop</span>
       </Button>
+
       <ClaimAirdropDialog
         onPost={async () => {
           setIsLoading(true);
