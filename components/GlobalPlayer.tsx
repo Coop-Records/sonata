d08ece89ba4ef4a68c812cbe166c7184b2dbde28
TahirHameed74 <tsb_125@yahoo.com>
2024-06-05 04:01:15 +0500

    Player - keep button location consistent

--- components/GlobalPlayer.tsx
+++ components/GlobalPlayer.tsx
@@ -63,7 +63,7 @@ export default function GlobalPlayer() {
       data-type={metadata.type}
       className="sticky bottom-0 left-0 mt-auto w-screen space-y-6 bg-white py-3 shadow-2xl shadow-black"
     >
-      <div className="container flex items-center gap-3">
+      <div className=" relative container flex items-center gap-3">
         <div className="relative my-auto size-16 overflow-hidden rounded-lg shadow-md">
           <Image
             src={metadata.artworkUrl}
@@ -74,35 +74,36 @@ export default function GlobalPlayer() {
           />
         </div>
 
-        <div className="space-y-0.5 self-center">
+        <div className=" max-w-[33%] space-y-0.5 self-center">
           <div className="line-clamp-2 text-sm font-bold">{metadata.trackName}</div>
           <div className="line-clamp-2 text-xs font-extralight text-muted-foreground">
             {metadata.artistName}
           </div>
         </div>
+        <div className="absolute left-1/4 w-full">
+          <div className="ml-6 flex max-w-2xl grow flex-col items-center gap-1">
+            <div className="flex items-center">
+              <Button onClick={handlePrev} className="bg-white">
+                <Image src="/images/skip-back.png" width={22} height={22} alt="" />
+              </Button>
 
-        <div className="ml-6 flex max-w-2xl grow flex-col items-center gap-1">
-          <div className="flex items-center">
-            <Button onClick={handlePrev} className="bg-white">
-              <Image src="/images/skip-back.png" width={22} height={22} alt="" />
-            </Button>
-
-            <Button
-              onClick={player.playing ? handlePause : handlePlay}
-              variant="ghost"
-              className="rounded-full p-0"
-            >
-              {player.playing ? (
-                <MdPauseCircle className="text-4xl" />
-              ) : (
-                <MdPlayCircle className="text-4xl" />
-              )}
-            </Button>
-            <Button onClick={handleNext} className="bg-white">
-              <Image src="/images/skip-forward.png" width={22} height={22} alt="" />
-            </Button>
+              <Button
+                onClick={player.playing ? handlePause : handlePlay}
+                variant="ghost"
+                className="rounded-full p-0"
+              >
+                {player.playing ? (
+                  <MdPauseCircle className="text-4xl" />
+                ) : (
+                  <MdPlayCircle className="text-4xl" />
+                )}
+              </Button>
+              <Button onClick={handleNext} className="bg-white">
+                <Image src="/images/skip-forward.png" width={22} height={22} alt="" />
+              </Button>
+            </div>
+            <Scrubber className="w-full max-md:hidden" />
           </div>
-          <Scrubber className="w-full max-md:hidden" />
         </div>
       </div>
 
