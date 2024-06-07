d40ea0603988a9e936a998f8bb4a49d2212465f7
TahirHameed74 <tsb_125@yahoo.com>
2024-06-07 20:51:33 +0500

    time - 60+ minutes

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,9 +10,11 @@ export function cn(...inputs: ClassValue[]) {
 
 export function formatDuration(duration: number) {
   const durationSeconds = Math.floor(duration / 1000);
-  const minutes = Math.floor(durationSeconds / 60);
+  const hours = Math.floor(durationSeconds / 3600);
+  const minutes = Math.floor((durationSeconds % 3600) / 60);
   const seconds = Math.floor(durationSeconds % 60);
-  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
 export const formatBigInt = (balance: bigint | string): string => {
