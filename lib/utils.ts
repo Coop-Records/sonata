3c556da3d2b8acfb927ad4b2fcc552113fa0ef4f
TahirHameed74 <tsb_125@yahoo.com>
2024-06-07 22:32:55 +0500

    added louder channel

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,11 +10,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export function formatDuration(duration: number) {
   const durationSeconds = Math.floor(duration / 1000);
-  const hours = Math.floor(durationSeconds / 3600);
-  const minutes = Math.floor((durationSeconds % 3600) / 60);
+  const minutes = Math.floor(durationSeconds / 60);
   const seconds = Math.floor(durationSeconds % 60);
-
-  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
 }
 
 export const formatBigInt = (balance: bigint | string): string => {
