896be76a3608d8e06afd0cc6bf6209866bbe7c67
TahirHameed74 <tsb_125@yahoo.com>
2024-06-13 20:05:57 +0500

    channel house music

--- lib/consts.tsx
+++ lib/consts.tsx
@@ -38,7 +38,7 @@ export const CHANNELS = [
   { label: '/bangers', value: 'bangers', icon: 'https://i.imgur.com/URBvewb.png' },
   { label: '/spotify', value: 'spotify', icon: 'https://i.imgur.com/3Z8YjMT.jpg' },
   { label: '/coop-recs', value: 'coop-recs', icon: 'https://i.imgur.com/eecb7AP.gif' },
-  { label: '/housemusic', value: 'house-music', icon: 'https://i.imgur.com/rt1dcOI.jpg' },
+  { label: '/housemusic', value: 'housemusic', icon: 'https://i.imgur.com/rt1dcOI.jpg' },
 ];
 
 export const fetchPostsLimit = 20;
