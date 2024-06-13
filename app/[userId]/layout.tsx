ee661e0d170dfd03864a39385c09e9321016dab1
Hakob Yeroyan <hakob.yeroyan1@esterox.am>
2024-06-13 22:11:22 +0400

    fix: design changes

--- app/[userId]/layout.tsx
+++ app/[userId]/layout.tsx
@@ -6,53 +6,50 @@ import { Sheet, SheetContent } from '@/components/ui/sheet';
 import { useUi } from '@/providers/UiProvider';
 import GlobalPlayer from '@/components/GlobalPlayer';
 import FeedProvider from '@/providers/FeedProvider';
-import ClaimAirdropButton from "@/components/ClaimAirdropButton/ClaimAirdropButton";
-import {Avatar, AvatarImage} from "@/components/ui/avatar";
-import Link from "next/link";
-import {ArrowLeft} from "lucide-react";
+import ClaimAirdropButton from '@/components/ClaimAirdropButton/ClaimAirdropButton';
+import Link from 'next/link';
+import { ChevronLeft } from 'lucide-react';
+import { useNeynarProvider } from '@/providers/NeynarProvider';
+import UserMenu from '@/components/Header/UserMenu';
 
 export default function FeedLayout({ children }: { children: ReactNode }) {
-    const { menuOpen, setMenuOpen } = useUi();
+  const { menuOpen, setMenuOpen } = useUi();
+  const { user } = useNeynarProvider();
 
-    return (
-        <FeedProvider>
-            <div className="flex grow flex-col">
-                <meta property="of:accepts:xmtp" content="2024-02-01" />
-                <div className="flex grow">
-                    <nav className="md:hidden">
-                        <Sheet open={menuOpen} onOpenChange={setMenuOpen}>
-                            <SheetContent side="left">
-                                <Sidebar />
-                            </SheetContent>
-                        </Sheet>
-                    </nav>
+  return (
+    <FeedProvider>
+      <div className="flex grow flex-col">
+        <meta property="of:accepts:xmtp" content="2024-02-01" />
+        <div className="flex grow">
+          <nav className="md:hidden">
+            <Sheet open={menuOpen} onOpenChange={setMenuOpen}>
+              <SheetContent side="left">
+                <Sidebar />
+              </SheetContent>
+            </Sheet>
+          </nav>
 
-                    <nav className="shadow-xl max-md:hidden">
-                        <Sidebar />
-                    </nav>
+          <nav className="shadow-xl max-md:hidden">
+            <Sidebar />
+          </nav>
 
-                    <main className="flex grow flex-col">
-                        <div className="flex justify-between items-center px-20 pt-12">
-                <       Link href={'/'} className="flex item-center gap-1" ><ArrowLeft size={24} /> Home</Link>
-                        <div className='flex items-center gap-2'>
-                            <ClaimAirdropButton/>
-                            <Avatar className="size-8">
-                                <AvatarImage src={'images/user.png'}/>
-                            </Avatar>
-                        </div>
-                        </div>
-
-
-                                <div className="container mx-auto max-w-3xl space-y-6">
-                                    {children}
-                                </div>
-
-
-                    </main>
-                </div>
-
-                <GlobalPlayer/>
+          <main className="flex grow flex-col">
+            <div className="flex justify-between max-w-3xl w-full px-2 mx-auto items-center pt-12">
+              <Link href={'/'} className="flex item-center gap-1 font-semibold text-[#333536]">
+                <ChevronLeft size={24} /> Home
+              </Link>
+              <div className="flex items-center gap-2">
+                {user && <ClaimAirdropButton />}
+                {user && <UserMenu />}
+              </div>
             </div>
-        </FeedProvider>
-    );
+
+            <div className="container mx-auto max-w-3xl space-y-6">{children}</div>
+          </main>
+        </div>
+
+        <GlobalPlayer />
+      </div>
+    </FeedProvider>
+  );
 }
