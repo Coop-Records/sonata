c752685ccc88fb120a958483db5f57cd80309cb3
codesage <laughingsagecoder@gmail.com>
2024-06-13 22:34:20 +0200

    removing the loop

--- app/api/getChannelCasts/route.ts
+++ app/api/getChannelCasts/route.ts
@@ -1,32 +1,13 @@
 import getChannelsCasts from '@/lib/neynar/getChannelCasts';
-import { createClient } from '@supabase/supabase-js';
 import { NextRequest, NextResponse } from 'next/server';
 
-const SUPABASE_URL = process.env.SUPABASE_URL!;
-const SUPABASE_KEY = process.env.SUPABASE_KEY!;
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false,
-    detectSessionInUrl: false,
-  },
-});
 
 export async function GET(req: NextRequest) {
   const id = req.nextUrl.searchParams.get('channelId');
 
   if (!id) return NextResponse.json({ message: 'channelId is required' }, { status: 422 });
 
-  const { data: cast_query_date } = await supabase
-    .from('cast_query_date')
-    .select('lastcheck')
-    .eq('id', 1)
-    .single();
-
-  const date = cast_query_date ? new Date(cast_query_date.lastcheck): undefined;
-
-  const data = await getChannelsCasts(id, date);
+  const data = await getChannelsCasts(id);
 
   if (!Array.isArray(data)) return NextResponse.json(data, { status: 400 });
 
--- lib/neynar/getChannelCasts.ts
+++ lib/neynar/getChannelCasts.ts
@@ -3,58 +3,43 @@ import 'server-only';
 import { ErrorRes, FeedType, FilterType, NeynarV2APIClient as NeynarAPIClient } from '@neynar/nodejs-sdk/build/neynar-api/v2';
 
 
-const getChannelCasts = async (
-  channelId: string,
-  lastChecked = new Date(new Date().getTime() - 2 * 60 * 1000),
-  limit = 100
-) => {
+const getChannelCasts = async (channelId: string, limit = 100) => {
   const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
 
   const casts = [];
   let error: ErrorRes | undefined;
-  let cursor: string | null | undefined = undefined;
 
-  mainLoop: do {
-    try {
-      const result = await client.fetchFeed(
-        FeedType.Filter,
-        {
-          filterType: FilterType.ChannelId,
-          withRecasts: false,
-          channelId,
-          limit,
-          cursor,
-        }
-      );
-
-      for (const entry of result.casts) {
-        if (new Date(entry.timestamp) > lastChecked) {
-
-          const found = entry.embeds.some((embed: any): boolean => {
-            const url = embed?.url as string | undefined;
-
-            if (!url) return false;
-
-            return (
-              url.includes('youtube.com/watch') ||
-              url.includes('soundcloud.com') ||
-              url.includes('sound.xyz') ||
-              url.includes('spotify.com/track')
-            );
-          });
-
-          if (found) casts.push(entry);
-        }
-        else break mainLoop;
+  try {
+    const result = await client.fetchFeed(
+      FeedType.Filter,
+      {
+        filterType: FilterType.ChannelId,
+        withRecasts: false,
+        channelId,
+        limit
       }
+    );
 
-      cursor = result.next.cursor;
+    for (const entry of result.casts) {
+      const found = entry.embeds.some((embed: any): boolean => {
+        const url = embed?.url as string | undefined;
 
-    } catch (err: any) {
-      error = err;
-      break;
+        if (!url) return false;
+
+        return (
+          url.includes('youtube.com/watch') ||
+          url.includes('soundcloud.com') ||
+          url.includes('sound.xyz') ||
+          url.includes('spotify.com/track')
+        );
+      });
+
+      if (found) casts.push(entry);
     }
-  } while (cursor);
+
+  } catch (err: any) {
+    error = err;
+  }
 
   if (error) return error;
   return casts;
