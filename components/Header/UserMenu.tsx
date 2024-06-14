78c960392d07b52ff9bd1e966100783bda1ab84d
TahirHameed74 <tsb_125@yahoo.com>
2024-06-14 21:54:25 +0500

    Add link to personal profile on the top right menu

--- components/Header/UserMenu.tsx
+++ components/Header/UserMenu.tsx
@@ -3,7 +3,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
