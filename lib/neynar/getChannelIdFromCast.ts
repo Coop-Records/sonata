543a1e1a8ea6590220f1245766e5c688ebb608ca
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-17 17:29:42 -0500

    I allow indexing of root_parent_url in casts for channels like /classical

--- lib/neynar/getChannelIdFromCast.ts
+++ lib/neynar/getChannelIdFromCast.ts
@@ -3,14 +3,19 @@ import { CHANNELS } from '../consts';
 
 export default function getChannelIdFromCast(cast: Cast) {
   const parentUrl = cast.parent_url;
-  let channelId = null;
+  const rootParentUrl = (cast as any).root_parent_url
+
   if (parentUrl) {
     const match = /\/channel\/([^/]+)$/.exec(parentUrl);
     if (match) {
-      channelId = match[1];
-    } else {
-      channelId = CHANNELS.find((val) => val.parentUrl == parentUrl)?.value ?? null;
+      return match[1];
+    }  
+    if (parentUrl) {
+      return CHANNELS.find((val) => val.parentUrl == parentUrl)?.value ?? null;
+    }
+    if (rootParentUrl) {
+      return CHANNELS.find((val) => val.parentUrl == rootParentUrl)?.value ?? null;
     }
   }
-  return channelId;
+  return null;
 }
