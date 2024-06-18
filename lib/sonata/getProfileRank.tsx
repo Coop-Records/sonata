33cfc691c8db201891fe0c996b07ff4618b4471e
TahirHameed74 <tsb_125@yahoo.com>
2024-06-18 21:17:19 +0500

    updated leaderboard

--- lib/sonata/getProfileRank.tsx
+++ lib/sonata/getProfileRank.tsx
@@ -14,14 +14,18 @@ const getProfileRank = async (wallets: Address[]) => {
 
       const response = await fetch(`/api/getLeaderboardRank?${queryParams}`, options);
       const data = await response.json();
+
       return data.leaderBoard;
     });
 
     const rank = await Promise.all(rankPromise);
-    const rankLeaderBoard = rank.reduce(function (res, obj) {
-      return obj.rank < res.rank ? obj : res;
+    let lowestRank = Number.MAX_VALUE;
+    rank.filter((obj: any) => {
+      if (obj.rank && obj.rank < lowestRank) {
+        lowestRank = obj.rank;
+      }
     });
-    return rankLeaderBoard.rank;
+    return lowestRank;
   } catch (error) {
     console.error(error);
     return { error };
