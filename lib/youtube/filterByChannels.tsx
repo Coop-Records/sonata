8616892edb289f378a8a65b7b009637ef77832f5
codesage <laughingsagecoder@gmail.com>
2024-06-18 12:20:15 +0200

    removing repetition from youtube filterByChannels

--- lib/youtube/filterByChannels.tsx
+++ lib/youtube/filterByChannels.tsx
@@ -1,10 +1,10 @@
-import { CHANNELS } from '../consts';
+import { Cast } from '@neynar/nodejs-sdk/build/neynar-api/v2';
 import getChannelIdFromCast from '../neynar/getChannelIdFromCast';
 
-const filterByChannels = (youtube: any) =>
+const filterByChannels = (youtube: Cast[]) =>
   youtube.filter((entry: any) => {
     const channelId = getChannelIdFromCast(entry);
-    return channelId && CHANNELS.find((channel) => channel.value === channelId);
+    return channelId;
   });
 
 export default filterByChannels;
