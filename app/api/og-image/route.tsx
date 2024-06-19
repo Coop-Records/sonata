8ae298a09d4b3ea4035df1d03be34869da64bb2b
TahirHameed74 <tsb_125@yahoo.com>
2024-06-19 17:52:49 +0500

    dynamic SEO image for casts

--- app/api/og-image/route.tsx
+++ app/api/og-image/route.tsx
@@ -4,11 +4,6 @@ import { NextRequest } from 'next/server';
 
 export const runtime = 'edge';
 
-export const size = {
-  width: 1200,
-  height: 630,
-};
-
 export const contentType = 'image/png';
 
 export async function GET(req: NextRequest) {
