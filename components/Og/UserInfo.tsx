75ada19fd77c88341c694a85d0c9aa1d9d044e48
TahirHameed74 <tsb_125@yahoo.com>
2024-06-26 20:55:24 +0500

    fixed design layout on username and centered the box

--- components/Og/UserInfo.tsx
+++ components/Og/UserInfo.tsx
@@ -17,7 +17,7 @@ const UserInfo = ({ profilePfp, rank, encodedUsername }: any) => (
       {rank > 0 && (
         <div tw="flex items-center justify-center bg-[#F6F6F6] rounded-full w-[100px] ml-5">
           <p tw="text-2xl ml-2 m-0 text-[#333536]" style={{ fontWeight: 600 }}>
-            {rank}
+            #{rank}
           </p>
         </div>
       )}
