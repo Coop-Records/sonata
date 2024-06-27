675512d6a612bdcbaf004ccb994a4d7b0621095c
codesage <laughingsagecoder@gmail.com>
2024-06-27 12:19:12 +0200

    adding the channe/stats api to docs

--- components/ApiPage/ApiPage.tsx
+++ components/ApiPage/ApiPage.tsx
@@ -4,7 +4,8 @@ import Endpoint from './Endpoint';
 const ApiPage = () => (
   <div className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-gray-100">
     <ApiPageHeader />
-    <Endpoint apiType="GET" route="/api/feed/" />
+    <Endpoint apiType="GET" route="/api/feed" />
+    <Endpoint apiType="GET" route="/api/channel/stats" />
   </div>
 );
 
