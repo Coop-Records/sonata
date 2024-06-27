09d5899b1b1bfc2cd45cf0a2788ebce1fa9aa0f9
codesage <laughingsagecoder@gmail.com>
2024-06-27 18:47:40 +0200

    documenting supported query parameters

--- components/ApiPage/ApiPage.tsx
+++ components/ApiPage/ApiPage.tsx
@@ -4,7 +4,17 @@ import Endpoint from './Endpoint';
 const ApiPage = () => (
   <div className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-gray-100">
     <ApiPageHeader />
-    <Endpoint apiType="GET" route="/api/feed" />
+    <Endpoint
+      apiType="GET"
+      route="/api/feed"
+      queryParams={[
+        'feedType=Recent',
+        'feedType=Trending',
+        'feedType=Following',
+        'viewerFid=<Farcaster ID>',
+        'channelId=<Farcaster Channel ID>',
+      ]}
+    />
     <Endpoint apiType="GET" route="/api/channel/stats" />
   </div>
 );
--- components/ApiPage/Endpoint.tsx
+++ components/ApiPage/Endpoint.tsx
@@ -1,7 +1,17 @@
-const Endpoint = ({ apiType, route }: any) => (
-  <div className="flex w-1/2 items-center justify-start rounded-lg bg-gray-200 p-8 shadow">
-    <div className="font-bold">{apiType}</div>
-    <div> - {route}</div>
+interface PropTypes {
+  apiType: string;
+  route: string;
+  queryParams?: string[],
+};
+
+const Endpoint = ({ apiType, route, queryParams }: PropTypes) => (
+  <div className="w-1/2 space-y-4 rounded-lg bg-gray-200 p-8 shadow">
+    <div><b className="font-bold">{apiType}</b> - {route}</div>
+    {queryParams?.map((query) => (
+      <div key={route + query}>
+        <b className="font-bold">{apiType}</b> - {route}?{query}
+      </div>
+    ))}
   </div>
 );
 
