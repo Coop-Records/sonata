250b059a6d530be0b91e2ee431be98055fe84bf0
TahirHameed74 <tsb_125@yahoo.com>
2024-06-27 20:41:34 +0500

    updated endpoint lists

---
+++ lib/getEndpointsList.tsx
@@ -0,0 +1,9 @@
+export const endpoints = [
+  { apiType: 'GET', route: '/api/feed' },
+  { apiType: 'GET', route: '/api/feed?feedType=Recent' },
+  { apiType: 'GET', route: '/api/feed?feedType=Trending' },
+  { apiType: 'GET', route: '/api/feed?feedType=Following&viewerFid=<Farcaster ID>' },
+  { apiType: 'GET', route: '/api/feed?viewerFid=<Farcaster ID>' },
+  { apiType: 'GET', route: '/api/feed?channelId=<Farcaster Channel ID>' },
+  { apiType: 'GET', route: '/api/channel/stats' },
+];
