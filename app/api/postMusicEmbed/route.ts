671d8f8b925f44ac7fcbd25b15dbd902bfba4aa8
codesage <laughingsagecoder@gmail.com>
2024-07-01 16:14:26 +0200

    awaiting post creation & using author field from new cast

--- app/api/postMusicEmbed/route.ts
+++ app/api/postMusicEmbed/route.ts
@@ -1,24 +1,18 @@
 import postMusicEmbed from '@/lib/neynar/postMusicEmbed';
 import sendBotCast from '@/lib/sonata/sendBotCast';
 import upsertCast from '@/lib/supabase/upsertCast';
-import { NeynarV2APIClient } from '@neynar/nodejs-sdk/build/neynar-api/v2';
 import { NextRequest } from 'next/server';
-
-const client = new NeynarV2APIClient(process.env.NEYNAR_API_KEY!)
 
 const getResponse = async (req: NextRequest) => {
   const body = await req.json();
   const { signer_uuid, url } = body;
 
   const data = await postMusicEmbed(signer_uuid, url);
-  console.log('postMusicEmbed', 'new cast:', data);
+  console.log('new cast:', data);
 
   try {
     if (!data?.cast?.hash) throw new Error('No hash provided');
-    if (!data.cast?.author?.fid) throw new Error('No author fid provided');
-
-    const fid = Number(data.cast.author.fid);
-    const { users } = await client.fetchBulkUsers([fid]);
+    if (!data.cast?.author?.fid) throw new Error('No author provided');
 
     const cast: any = {
       timestamp: new Date().toISOString(),
@@ -29,15 +23,14 @@ const getResponse = async (req: NextRequest) => {
       },
       hash: data.cast.hash,
       embeds: [{ url }],
-      author: users[0],
+      author: data.cast.author,
     };
-    upsertCast(cast);
+    await upsertCast(cast);
     sendBotCast(cast);
 
   } catch (error) {
-    console.error('api/postMusicEmbed::Error', error);
+    console.error('Error:', error);
   }
-
   return Response.json({ message: 'success', data }, { status: 200 });
 };
 
