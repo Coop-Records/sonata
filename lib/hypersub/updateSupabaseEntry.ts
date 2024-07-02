28d4030049265afe6179caaa06d6402d79bbdeef
codesage <laughingsagecoder@gmail.com>
2024-07-02 17:47:59 +0200

    adding where clause to update

--- lib/hypersub/updateSupabaseEntry.ts
+++ lib/hypersub/updateSupabaseEntry.ts
@@ -2,9 +2,11 @@ import { supabaseServerClient } from "../supabase/serverClient";
 
 const updateSupabaseEntry = (
   fid: number, hasBalance: boolean
-) => supabaseServerClient.from('tips').update({
-  fid,
-  hypersub_subscribed_since: hasBalance ? new Date().toISOString() : null
-});
+) => supabaseServerClient.from('tips')
+  .update({
+    hypersub_subscribed_since: hasBalance ? new Date().toISOString() : null
+  })
+  .eq('fid', fid)
+  .is('hypersub_subscribed_since', null);
 
 export default updateSupabaseEntry;
