16517ec1bf6c6fb9cf7952a6949c73f25f9c80e6
codesage <laughingsagecoder@gmail.com>
2024-07-02 11:46:59 +0200

    returning all channels in stats

--- app/api/channel/stats/route.ts
+++ app/api/channel/stats/route.ts
@@ -1,15 +1,31 @@
+import { CHANNELS } from "@/lib/consts";
 import combinePrivyAccountWithChannelStats from "@/lib/privy/combineAccountsWithStats";
 import getChannelStats from "@/lib/supabase/getChannelStats";
+import { ChannelStats } from "@/types/ChannelStats";
 
 export async function GET() {
   try {
-    const channelStats = await getChannelStats(false, true);
+    const channelStats = await getChannelStats();
     console.log('channelStats count', channelStats.length);
 
-    const channels = await combinePrivyAccountWithChannelStats(channelStats);
-    console.log('channelStatsAddresses count', channels.length);
+    const splitChannelStats = channelStats.reduce<
+      [ChannelStats[], ChannelStats[]]
+    >((previous, current) => {
+      const inChannels = CHANNELS.some(channel => channel.value === current.channelId);
+      if (inChannels) previous[0].push(current);
+      else previous[1].push(current);
 
-    return Response.json({ message: 'success', channels }, { status: 200 });
+      return previous;
+    }, [[], []]);
+
+    const channelStatsWithAddresses =
+      await combinePrivyAccountWithChannelStats(splitChannelStats[0]);
+    console.log('channelStatsWithAddresses count', channelStatsWithAddresses.length);
+
+    return Response.json({
+      message: 'success',
+      channels: [...channelStatsWithAddresses, ...splitChannelStats[1]]
+    });
   } catch (error) {
     console.error('Error:', error);
     return Response.json({ message: 'failed' }, { status: 400 });
