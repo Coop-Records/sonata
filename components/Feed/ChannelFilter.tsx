d95df10cdaa66fc53f6b392cbef0b405d50bfcb6
codesage <laughingsagecoder@gmail.com>
2024-07-12 02:58:04 +0200

    updating the channels list sidebar

--- components/Feed/ChannelFilter.tsx
+++ components/Feed/ChannelFilter.tsx
@@ -17,7 +17,7 @@ export default function ChannelFilter() {
   return (
     <div className="flex grow flex-col">
       <h2 className="mb-2 font-semibold">Channels</h2>
-      <div className='grow basis-0 overflow-y-auto scrollbar-thin md:-mr-16'>
+      <div className='grow basis-0 overflow-y-auto scrollbar-thin md:-mr-16 md:pr-12'>
         {menuItems.map((option) => {
           const active = currentFilter?.channel === option.value;
           return (
@@ -26,8 +26,8 @@ export default function ChannelFilter() {
               onClick={() => handleClick(option.value)}
               key={option.value}
               className={cn(
-                'flex px-3 py-2 justify-start items-center space-x-2 w-full font-semibold',
-                active && 'bg-muted',
+                'flex px-6 py-2 justify-start items-center gap-2 w-full font-semibold text-sm',
+                active && 'rounded-xl bg-grey-light',
               )}
             >
               <div className="relative size-6 overflow-hidden rounded-full">
--- components/Header/HomeButton.tsx
+++ components/Header/HomeButton.tsx
@@ -1,29 +1,13 @@
-import { useFeedProvider } from '@/providers/FeedProvider';
-import { FeedType } from '@/types/Feed';
-import { useRouter } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { ChevronLeft } from 'lucide-react';
+import Link from 'next/link';
 
-const HomeButton = () => {
-  const { push } = useRouter();
-  const { setFeedType } = useFeedProvider();
-
-  const onClick = () => {
-    setFeedType(FeedType.Trending);
-    push('/');
-  };
-
+const HomeButton = ({ className}: { className?: string }) => {
   return (
-    <button className="flex w-full items-center gap-3" type="button" onClick={onClick}>
-      <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M7 13L1 7L7 1"
-          stroke="#333536"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+    <Link href='/' className={cn("flex items-center gap-1", className)}>
+      <ChevronLeft/>
       <p className="font-sora text-[18px] font-semibold">Home</p>
-    </button>
+    </Link>
   );
 };
 
