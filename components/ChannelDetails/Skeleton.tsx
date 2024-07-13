62d184848582be22a17511ab2b04b6efde13d161
codesage <laughingsagecoder@gmail.com>
2024-07-13 17:38:47 +0200

    shorter height on the skeleton text loader

--- components/ChannelDetails/Skeleton.tsx
+++ components/ChannelDetails/Skeleton.tsx
@@ -6,32 +6,32 @@ function ChannelDetailsSkeleton({ channelId = '' }: { channelId: string }) {
     <div className='flex flex-wrap justify-between gap-x-3 gap-y-6 max-md:mt-3'>
       <div>
         <h1 className='text-2xl font-semibold'>/{channelId}</h1>
-        <Skeleton className='h-6 w-52' />
+        <Skeleton className='h-4 w-52' />
 
         <div className='mt-4 flex flex-wrap items-end gap-x-6 gap-y-3'>
           <div>
-            <Skeleton className='h-6 w-full' />
+            <Skeleton className='h-4 w-full' />
             <span className="col-span-full text-sm text-grey">Notes</span>
           </div>
 
           <div>
-            <Skeleton className='h-6 w-full' />
+            <Skeleton className='h-4 w-full' />
             <span className="col-span-full text-sm text-grey">Staked</span>
           </div>
 
           <div>
-            <Skeleton className='h-6 w-full' />
+            <Skeleton className='h-4 w-full' />
             <p className="col-span-full text-sm text-grey">Stakers</p>
           </div>
 
           <div className='grid grid-cols-[auto_1fr] items-center gap-x-1'>
-            <Skeleton className='size-6' />
-            <Skeleton className='h-6 w-16' />
+            <Skeleton className='size-4' />
+            <Skeleton className='h-4 w-16' />
             <span className="col-span-full text-sm text-grey">Top Song</span>
           </div>
 
           <div>
-            <Skeleton className='h-6 w-full' />
+            <Skeleton className='h-4 w-full' />
             <p className="col-span-full text-sm/4 text-grey">Moderators</p>
           </div>
         </div>
