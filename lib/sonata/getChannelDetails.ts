9d3f1cfe542ee462f92d44ef5078fd2139248201
codesage <laughingsagecoder@gmail.com>
2024-07-16 18:39:18 +0200

    removing get user stake details if not signed in

--- lib/sonata/getChannelDetails.ts
+++ lib/sonata/getChannelDetails.ts
@@ -6,11 +6,14 @@ async function getChannelDetails(id: string, fid: number | undefined) {
 
   const processSuccess = (res: Response) => res.ok ? res.json() : null;
 
-  const [info, stats, user] = await Promise.all([
+  const promises = [
     fetch(`/api/neynar/getChannelDetails?channelId=${id}`).then(processSuccess),
-    fetch(`/api/channel/details?channelId=${id}`).then(processSuccess),
-    fetch(`/api/channel/stake?channelId=${id}&fid=${fid ?? ''}`).then(processSuccess)
-  ]);
+    fetch(`/api/channel/details?channelId=${id}`).then(processSuccess)
+  ];
+  if (fid) promises.push(
+    fetch(`/api/channel/stake?channelId=${id}&fid=${fid}`).then(processSuccess)
+  );
+  const [info, stats, user] = await Promise.all(promises);
 
   const embed = stats?.topSong ? findValidEmbed(stats.topSong) : null;
   const metadata = embed ? await fetchMetadata(embed.url, stats.topSong) : undefined;
