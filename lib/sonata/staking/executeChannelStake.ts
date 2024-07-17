ec16cb905b31968d2a77f339d2089264ac2f30b9
codesage <laughingsagecoder@gmail.com>
2024-07-17 22:21:40 +0200

    subtracting balance from staker

--- lib/sonata/staking/executeChannelStake.ts
+++ lib/sonata/staking/executeChannelStake.ts
@@ -5,31 +5,33 @@ import getChannelTipInfo from "../tip/getChannelTipInfo";
 
 async function executeChannelStake(channelId: string, stakeAmount: number, fid: number) {
   const user = await getUser(fid);
+  if (!user?.verifications?.[0]) throw Error('No user address found');
 
-  const balances: { amount: number }[] = await stack.getPoints(
+  const balances: { amount: number; address: string }[] = await stack.getPoints(
     user.verifications.map((verification: string) => verification)
   );
-  const currentBalance = balances.reduce((prev, curr) => prev + curr.amount, 0);
+  const balance = balances.find(balance => balance.amount >= stakeAmount);
+  if (!balance) throw Error('No balances found');
 
-  const amount = Math.min(currentBalance, stakeAmount);
+  const amount = Math.min(balance.amount, stakeAmount);
 
   const info = await getChannelTipInfo(channelId, 0);
   if (!info) throw Error('could not find channel');
 
   const { channelAddress } = info;
 
-  const { success } = await stack.track(
-    `channel_stake_from_${fid}`,
-    { account: channelAddress, points: amount }
-  );
-  if (!success) throw Error('Could not stack');
+  const results = await Promise.all([
+    stack.track(`channel_stake_from_${fid}`, { account: balance.address, points: -amount }),
+    stack.track(`channel_stake_on_${channelAddress}`, { account: channelAddress, points: amount }),
+  ]);
+  if (results.some(res => !res.success)) throw Error('Could not stack');
 
   const { error } = await supabase
     .from('stake_activity_log')
     .insert({ fid, amount, channelId, channelAddress });
   if (error) console.error('executeChannelStake', error);
 
-  return { usedAmount: amount, remainingBalance: currentBalance - amount };
+  return { usedAmount: amount, remainingBalance: balance.amount - amount };
 }
 
 export default executeChannelStake;
