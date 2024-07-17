de3ec0d4b57f9ff0a22b558742fe1c3413688d3a
codesage <laughingsagecoder@gmail.com>
2024-07-17 17:23:28 +0200

    forcing no-cache & diversifying user tips on api/tips

--- lib/getLatestTips.ts
+++ lib/getLatestTips.ts
@@ -11,7 +11,8 @@ export const getLatestTips = async (): Promise<NextResponse> => {
     .from('tips_activity_log')
     .select('*')
     .order('created_at', { ascending: false })
-    .limit(11);
+    .limit(100);
+
 
   if (error) {
     return NextResponse.json(
@@ -19,9 +20,23 @@ export const getLatestTips = async (): Promise<NextResponse> => {
       { status: 500 },
     );
   }
+  const LIMIT = 11;
+  const MAX_SENDER_APPEARANCE = 2;
+  const result = [];
+  const senderCounts = new Map<string, number>();
 
+  for (const item of data) {
+    const count = senderCounts.get(item.sender) ?? 0;
+
+    if (count < MAX_SENDER_APPEARANCE) {
+      senderCounts.set(item.sender, count + 1);
+      result.push(item);
+
+      if (result.length >= LIMIT) break;
+    }
+  }
   return NextResponse.json(
-    { message: 'Latest tips retrieved successfully', tips: data },
+    { message: 'Latest tips retrieved successfully', tips: result },
     { status: 200 },
   );
 };
