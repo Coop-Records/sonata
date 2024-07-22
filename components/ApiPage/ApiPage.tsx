e723e93fde038308d36b13df41136c75a220eaa0
codesage <laughingsagecoder@gmail.com>
2024-07-22 14:03:01 +0200

    documenting channel details, stake, & unstake api

--- components/ApiPage/ApiPage.tsx
+++ components/ApiPage/ApiPage.tsx
@@ -3,7 +3,7 @@ import ApiPageHeader from './ApiPageHeader';
 import Endpoint from './Endpoint';
 
 const ApiPage = () => (
-  <div className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-gray-100">
+  <div className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-gray-100 py-8">
     <ApiPageHeader />
     {endpoints.map((endpoint, index) => (
       <Endpoint key={index} apiType={endpoint.apiType} route={endpoint.route} />
