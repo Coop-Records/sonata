8d728aece92748b1400167eb28651b0d6180c3f5
Sweets Sweetman <sweetmantech@gmail.com>
2024-07-28 00:58:04 -0300

    I remove unused const.

--- lib/consts.tsx
+++ lib/consts.tsx
@@ -27,7 +27,6 @@ export const tabs = [
 
 export const CHAIN = zora;
 export const CHAIN_ID = zora.id;
-export const NEWTRO_ARTS_ADDRESS = '0x33912a0d6bEFf5Fb8e5B70688CE858D5e7E8104E';
 export const NEWTRO_FIRST_ZORA_BLOCK = BigInt(2671653);
 
 export const CHANNELS = [
--- lib/zora/getSetupNewContractLogs.tsx
+++ lib/zora/getSetupNewContractLogs.tsx
@@ -1,4 +1,4 @@
-import { CHAIN_ID, NEWTRO_ARTS_ADDRESS, NEWTRO_FIRST_ZORA_BLOCK } from '../consts';
+import { CHAIN_ID, NEWTRO_FIRST_ZORA_BLOCK } from '../consts';
 import {
   zoraCreator1155FactoryImplAddress,
   zoraCreator1155FactoryImplABI,
@@ -8,13 +8,12 @@ import { getPublicClient } from '../viem';
 const getSetupNewContractLogs = async () => {
   const fromBlock = NEWTRO_FIRST_ZORA_BLOCK;
   const publicClient = getPublicClient(CHAIN_ID);
+  const args = {};
   const contractEvents = await publicClient.getContractEvents({
     address: zoraCreator1155FactoryImplAddress[CHAIN_ID],
     abi: zoraCreator1155FactoryImplABI,
     eventName: 'SetupNewContract',
-    args: {
-      // creator: NEWTRO_ARTS_ADDRESS,
-    },
+    args,
     fromBlock,
   });
   return contractEvents;
