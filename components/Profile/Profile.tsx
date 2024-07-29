fee8a4b17113804c371cbfce4e59ae155e822a29
codesage <laughingsagecoder@gmail.com>
2024-07-29 21:05:35 +0200

    removing profile header center alignment

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -3,23 +3,17 @@ import Notes from './Notes';
 import Songs from './Songs';
 import TopSong from './TopSong';
 import Followers from './Followers';
-import { useFeedProvider } from '@/providers/FeedProvider';
-import { FeedType } from '@/types/Feed';
-import { cn } from '@/lib/utils';
 
-const Profile = () => {
-  const { feedType } = useFeedProvider();
-  return (
-    <section className={cn("container mx-auto mb-4 mt-8 space-y-4", { "max-w-3xl": feedType !== FeedType.Stakes })}>
-      <BaseInfo />
-      <div className="flex gap-4">
-        <Songs />
-        <Notes />
-        <TopSong />
-      </div>
-      <Followers />
-    </section>
-  );
-};
+const Profile = () => (
+  <section className="container mx-auto mb-4 mt-8 space-y-4">
+    <BaseInfo />
+    <div className="flex gap-4">
+      <Songs />
+      <Notes />
+      <TopSong />
+    </div>
+    <Followers />
+  </section>
+);
 
 export default Profile;
