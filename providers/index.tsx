d77822fc883901e6393bfd55627262aeb5a6c2b6
codesage <laughingsagecoder@gmail.com>
2024-07-30 20:49:18 +0200

    resolving slow channel loading

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,20 +1,17 @@
 'use client';
 
 import NeynarProvider from '@/providers/NeynarProvider';
+import AudioProvider from './audio';
 import TipProvider from './TipProvider';
 import UiProvider from './UiProvider';
-import AudioProvider from './audio';
-import ChannelStakeProvider from './StakeProvider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NeynarProvider>
       <TipProvider>
-        <ChannelStakeProvider>
-          <UiProvider>
-            <AudioProvider>{children}</AudioProvider>
-          </UiProvider>
-        </ChannelStakeProvider>
+        <UiProvider>
+          <AudioProvider>{children}</AudioProvider>
+        </UiProvider>
       </TipProvider>
     </NeynarProvider>
   );
