3146bdca2248329d737a607632ae4f132fbac00a
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-09 22:12:42 -0300

    I remove unused formatLogs lib in favor of the shared formatting lib.

--- app/api/zora/collections/route.ts
+++ app/api/zora/collections/route.ts
@@ -1,15 +1,15 @@
 import getSetupNewContractLogs from "@/lib/zora/getSetupNewContractLogs";
-import { formatLogs } from "./formatLogs";
 import { NextRequest } from "next/server";
 import { Address } from "viem";
 import trackEndpoint from "@/lib/stack/trackEndpoint";
+import formatBigIntValues from "@/lib/formatBigIntValues";
 
 export async function GET(request: NextRequest) {
   try {
     await trackEndpoint("zora+collections")
     const creator = new URL(request.url).searchParams.get('creator') as Address; 
     const logs = await getSetupNewContractLogs(creator)
-    const formattedLogs = formatLogs(logs);
+    const formattedLogs = formatBigIntValues(logs);
     return Response.json({ message: 'success', data: formattedLogs });
   } catch (error) {
     console.error('Error:', error);
