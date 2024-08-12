309c98eeb16f735f67ced43e586b1293839b9e79
codesage <laughingsagecoder@gmail.com>
2024-08-12 19:33:34 +0200

    explicitly passing amount of days(7)

--- app/api/jobs/channels/route.ts
+++ app/api/jobs/channels/route.ts
@@ -27,8 +27,8 @@ export async function GET(req: NextRequest) {
     const results = await Promise.all(sortedChannels.map(async (channel) => {
       const channelId = channel.channelId;
 
-      const balance = channel.staked ? await getDaysChannelTotalTips(channelId) : 0;
-      const TIPS = channel.staked ? Math.floor(balance / 2) : 0;
+      const balance = channel.staked ? await getDaysChannelTotalTips(channelId, 7) : 0;
+      const TIPS = channel.staked && balance > 0 ? Math.floor(balance / 2) : 0;
       const CHANNEL_AIRDROP = channel.staked ? Math.floor(AIRDROP_AMOUNT / 2) : AIRDROP_AMOUNT;
       const STAKERS_AIRDROP = channel.staked ? TIPS + CHANNEL_AIRDROP : 0;
 
