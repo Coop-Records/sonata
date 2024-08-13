9ba8fc9d086fe0f14fdfb6ef584f43dbd69fe131
Arpit Gupta <arpitgupta1214@gmail.com>
2024-08-13 19:51:41 +0530

    success image fix

--- app/api/og-image/frame-tip-result/route.tsx
+++ app/api/og-image/frame-tip-result/route.tsx
@@ -1,5 +1,5 @@
 import FrameTipResponse from '@/components/Og/FrameTipResponse';
-import { ImageResponse } from '@vercel/og';
+import { ImageResponse } from 'next/og';
 import { NextRequest } from 'next/server';
 
 export const runtime = 'edge';
