7127e20f6c6e78ea403e0c49f2811eec2ce17caa
codesage <laughingsagecoder@gmail.com>
2024-08-19 19:11:00 +0200

    creating hooks/useTabs & component/Tab

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,8 +1,8 @@
 'use client';
-import { Button } from '@/components/ui/button';
 import useQueryParams from '@/hooks/useQueryParams';
+import useTabs from '@/hooks/useTabs';
 import { cn } from '@/lib/utils';
-import { useCallback, useEffect, useState } from 'react';
+import Tab from './Tab';
 
 interface Props {
   tabs: {
@@ -15,42 +15,25 @@ interface Props {
 };
 
 export default function Tabs({ tabs, className = '', onChange }: Props) {
-  const { setQueryParam, queryParams } = useQueryParams();
-  const getTab = useCallback(() => {
-    const tab = queryParams.get('tab');
-    const found = tabs.findIndex(t => t.value === tab);
-    console.log({ found });
-
-    if (found >= 0) return found;
-    return 0;
-  }, [queryParams, tabs]);
-  const [active, setActive] = useState(getTab);
-
-  useEffect(() => {
-    const index = getTab();
-    setActive(index);
-  }, [tabs, getTab]);
+  const { setQueryParam } = useQueryParams();
+  const { activeTab, setActiveTab } = useTabs(tabs);
 
   const onTabChange = (value: string, index: number) => {
     if (onChange) onChange(value);
-    setActive(index);
+    setActiveTab(index);
     setQueryParam('tab', value);
-  }
+  };
 
   return (
     <ul className={cn('flex gap-4 md:gap-8', className)}>
-      {tabs.map((tab, index) => {
-        return (
-          <li className={cn(active === index && 'border-b-2 border-black')} key={index}>
-            <Button
-              variant="ghost"
-              className="p-0 text-sm font-bold hover:bg-transparent md:text-lg"
-              onClick={() => onTabChange(tab.value, index)}>
-              {tab.label}
-            </Button>
-          </li>
-        );
-      })}
+      {tabs.map((tab, index) => (
+        <Tab
+          tab={tab}
+          key={index}
+          className={cn(activeTab === index && 'border-b-2 border-black')}
+          onClick={() => onTabChange(tab.value, index)}
+        />
+      ))}
     </ul>
   );
 }
