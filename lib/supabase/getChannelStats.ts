34e12b1fb7dfebcefb448b3a49696f94e88c7a2c
codesage <laughingsagecoder@gmail.com>
2024-08-23 18:19:51 +0200

    modifying api/channel/stats includes fid param

--- lib/supabase/getChannelStats.ts
+++ lib/supabase/getChannelStats.ts
@@ -1,47 +1,14 @@
-import { ChannelAccumulator, ChannelStats } from "@/types/ChannelStats";
-import { CHANNELS } from "../consts";
+import { ChannelStats } from "@/types/ChannelStats";
 import extractAddresses from "../privy/extractAddresses";
 import getAllChannels from "../privy/getAllChannels";
 import getPrivyIdentifier from "../privy/getIdentifier";
 import getStackPoints from "../sonata/getStackPoints";
 import { eventStakeChannel, eventTipChannel } from "../stack/events";
+import getChannelAuthorsAndPosts from "./getChannelAuthorsAndPosts";
 import supabase from "./serverClient";
 
 async function getChannelStats(filterChannels = false) {
-  const limit = 1000;
-  let offset = 0;
-  const entries = {} as ChannelAccumulator;
-
-  do {
-    const query = supabase
-      .from('posts')
-      .select('channelId, post_hash, authorFid')
-      .range(offset, offset + limit - 1);
-
-    if (filterChannels) {
-      query.in('channelId', CHANNELS.map(channel => channel.value));
-    } else {
-      query.neq('channelId', null);
-    }
-
-    const { data: posts, error } = await query;
-
-    if (error) throw error;
-
-    posts.forEach(({ channelId, ...post }) => {
-      if (!entries[channelId]) entries[channelId] = {
-        uniquePosts: new Set,
-        uniqueAuthors: new Set,
-      };
-
-      entries[channelId].uniqueAuthors.add(post.authorFid);
-      entries[channelId].uniquePosts.add(post.post_hash);
-    });
-
-    if (posts.length < limit) break;
-    offset += limit;
-
-  } while (offset);
+  const entries = await getChannelAuthorsAndPosts(filterChannels);
 
   const wallets = await getAllChannels();
 
