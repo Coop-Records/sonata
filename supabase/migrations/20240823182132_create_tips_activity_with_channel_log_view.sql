abc0ab9a63c72d004e679afe89aefb4b47960977
codesage <laughingsagecoder@gmail.com>
2024-08-23 20:52:15 +0200

    updating sql in migration tips_activity_with_channel_log

--- supabase/migrations/20240823182132_create_tips_activity_with_channel_log_view.sql
+++ supabase/migrations/20240823182132_create_tips_activity_with_channel_log_view.sql
@@ -1,16 +1,7 @@
-CREATE
-OR REPLACE FUNCTION get_user_tipping_stats () RETURNS TABLE (total_users bigint, total_tippers bigint) AS $$
-BEGIN
-    RETURN QUERY 
-    SELECT 
-        COUNT(DISTINCT combined_users.fid) AS total_users,
-        COUNT(DISTINCT tips_activity_log.sender) AS total_tippers
-    FROM (
-        SELECT fid FROM tips
-        UNION
-        SELECT author->>'fid' AS fid FROM posts WHERE author IS NOT NULL
-    ) AS combined_users
-    LEFT JOIN tips_activity_log 
-    ON combined_users.fid = tips_activity_log.sender;
-END;
-$$ LANGUAGE plpgsql;
+CREATE VIEW tips_activity_with_channel_log AS
+SELECT
+  tal.*,
+  p."channelId" as channel_id
+FROM
+  tips_activity_log tal
+  LEFT JOIN posts p ON tal.post_hash = p.post_hash
