c3e4eb5c27046842e70b32e0b9b4914330a8eb62
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-30 19:08:55 -0300

    I init useTotalNotes for front end to use the new API.

--- hooks/useSongPage/index.ts
+++ hooks/useSongPage/index.ts
@@ -2,14 +2,14 @@ import useSongAlternatives from "./useSongAlternatives";
 import useSongCasts from "./useSongCasts";
 import useSongLink from "./useSongLink";
 import useSongMetadata from "./useSongMetadata";
+import useTotalNotes from "./useTotalNotes";
 
 export default function useSongPage() {
   const { songLink } = useSongLink();
   const { alternatives } = useSongAlternatives(songLink);
   const { metadata } = useSongMetadata(songLink);
   const { posts, loading } = useSongCasts(songLink, alternatives);
+  const {totalNotes} = useTotalNotes(songLink);
 
-  const totalNotes = loading ? undefined : posts.reduce((prev, curr) => prev + (curr.points ?? 0), 0);
-console.log("SWEETS GET totalNotes via API instead of client side", totalNotes)
   return { songLink, totalNotes, metadata, alternatives, posts, postsLoading: loading };
 }
