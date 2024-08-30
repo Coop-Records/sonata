eed2e52006f5dc0a56ff8278b22250d68638f110
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-30 19:10:53 -0300

    I cleanup the API route.

--- app/api/song/market/route.tsx
+++ app/api/song/market/route.tsx
@@ -4,24 +4,16 @@ import getSongLinks from '@/lib/songLink/getSongLinks';
 
 export async function GET(req: NextRequest) {
   const songLink = req.nextUrl.searchParams.get('songLink');
-
-  if (!songLink) {
-    return NextResponse.json({ error: 'songLink is required' }, { status: 400 });
-  }
-
+  if (!songLink) return NextResponse.json({ error: 'songLink is required' }, { status: 400 });
   try {
     const { data: alternatives } = await getSongLinks(songLink);
-    console.log('alternatives', alternatives);
     const embeds = Object.values(alternatives).concat(songLink);
-
     const { data: posts } = await supabaseClient
       .rpc('get_posts_by_embeds', { search_embeds: embeds })
       .select('*');
-
     const totalNotes = Array.isArray(posts)
       ? posts.reduce((prev, curr) => prev + (curr.points ?? 0), 0)
       : 0;
-
     return NextResponse.json({ totalNotes });
   } catch (error) {
     console.error('Error in /api/song/market:', error);
