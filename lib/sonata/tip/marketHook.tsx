5dc3517ad9885b985dba36afd3f6b99f7e0bcee9
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-31 19:05:13 -0300

    I remove dev logging.

--- lib/sonata/tip/marketHook.tsx
+++ lib/sonata/tip/marketHook.tsx
@@ -6,14 +6,11 @@ import trackSetupNewToken from '@/lib/stack/trackSetupNewToken';
 
 const marketHook = async (post: SupabasePost) => {
   const { totalNotes, songLinks } = await fetchSongMarketForPost(post);
-  console.log('totalNotes', totalNotes);
-  console.log('songLinks', songLinks);
-  if (totalNotes < MINIMUM_NOTES_FOR_SONG_MARKET) return;
+  const precheckApproved = totalNotes >= MINIMUM_NOTES_FOR_SONG_MARKET;
+  if (!precheckApproved) return;
   const response = await createSongToken();
   if (!response) return;
   const { newTokenId } = response;
-  console.log('SWEETS TOKENID', newTokenId);
-
   await trackSetupNewToken(Number(newTokenId), songLinks, post);
 };
 
