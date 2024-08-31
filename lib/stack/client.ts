339b75ba2877f8087bb0dcead6b7b88bd129d818
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-31 17:26:30 -0300

    I init getStackClient lib.

--- lib/stack/client.ts
+++ lib/stack/client.ts
@@ -4,3 +4,12 @@ export const stack = new StackClient({
   apiKey: process.env.STACK_API_KEY as string,
   pointSystemId: Number(process.env.STACK_SYSTEM_ID),
 });
+
+const getStackClient = (pointSystemId: number) => {
+  return new StackClient({
+    apiKey: process.env.STACK_API_KEY as string,
+    pointSystemId: pointSystemId,
+  });
+};
+
+export default getStackClient;
