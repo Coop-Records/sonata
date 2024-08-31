8f26c663d47b0a96e38d6e7e79f4b94611d1e23b
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-31 19:07:15 -0300

    I update stack client export to use new getStackClient lib.

--- lib/stack/client.ts
+++ lib/stack/client.ts
@@ -1,15 +1,12 @@
 import { StackClient } from '@stackso/js-core';
 
-export const stack = new StackClient({
-  apiKey: process.env.STACK_API_KEY as string,
-  pointSystemId: Number(process.env.STACK_SYSTEM_ID),
-});
-
 const getStackClient = (pointSystemId: number) => {
   return new StackClient({
     apiKey: process.env.STACK_API_KEY as string,
     pointSystemId: pointSystemId,
   });
 };
+
+export const stack = getStackClient(Number(process.env.STACK_SYSTEM_ID))
 
 export default getStackClient;
