2f118a637de85f1a06ea7276266c3fc7e2a52c6f
Sweets Sweetman <sweetmantech@gmail.com>
2024-08-31 15:58:47 -0300

    I rollback changes to lib/stack/getDaysChannelTotalTips.ts

--- lib/stack/getDaysChannelTotalTips.ts
+++ lib/stack/getDaysChannelTotalTips.ts
@@ -3,6 +3,7 @@ import getDaysEvents from "./getDaysEvents";
 
 async function getDaysChannelTotalTips(channelId: string, daysAgo = 7) {
   const events = await getDaysEvents(eventTipChannel(channelId), daysAgo);
+
   return events.reduce((prev, curr) => prev + curr.points, 0);
 }
 
