096775499f99c76f546b325b6cbe630160cbd49c
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-01 16:53:20 -0300

    I rollback changes to lib/sonata/song/getSongMarketCollection.

--- lib/sonata/song/getSongMarketCollection.tsx
+++ lib/sonata/song/getSongMarketCollection.tsx
@@ -4,8 +4,6 @@ import findMatchingEvent from './findMatchingEvent';
 
 async function getSongMarketCollection(songLinks: string[]): Promise<CollectionObject | null> {
   const setupNewTokenEvents = await fetchSetupNewTokenEvents();
-  console.log('SWEETS setupNewTokenEvents', setupNewTokenEvents[0].metadata);
-  console.log('SWEETS songLinks', songLinks);
   const matchedEvent = findMatchingEvent(setupNewTokenEvents, songLinks);
   return matchedEvent ? createCollectionObject(matchedEvent) : null;
 }
