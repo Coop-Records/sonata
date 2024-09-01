16223ce26bdc6c863af5b5b431839659c2771b55
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-01 00:40:25 -0300

    I simplify function logic.

--- lib/sonata/song/normalizeEmbedUrl.tsx
+++ lib/sonata/song/normalizeEmbedUrl.tsx
@@ -2,12 +2,8 @@ function normalizeEmbedUrl(url: string): string {
   const lowercaseUrl = url.toLowerCase();
   if (lowercaseUrl.includes('spotify.com')) {
     return lowercaseUrl.split('?')[0];
-  } else if (lowercaseUrl.includes('youtube.com') || lowercaseUrl.includes('youtu.be')) {
-    return lowercaseUrl;
-  } else {
-    // For other platforms, you might want to decide on a case-by-case basis
-    return lowercaseUrl;
   }
+  return lowercaseUrl;
 }
 
 export default normalizeEmbedUrl;
