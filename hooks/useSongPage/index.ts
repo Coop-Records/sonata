91f16aabdfde394921193e2ab492b7aa198e2fd0
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-01 16:51:26 -0300

    I refactor useTotalNotes => useSongMarket.

--- hooks/useSongPage/index.ts
+++ hooks/useSongPage/index.ts
@@ -2,14 +2,14 @@ import useSongAlternatives from "./useSongAlternatives";
 import useSongCasts from "./useSongCasts";
 import useSongLink from "./useSongLink";
 import useSongMetadata from "./useSongMetadata";
-import useTotalNotes from "./useTotalNotes";
+import useSongMarket from "./useSongMarket";
 
 export default function useSongPage() {
   const { songLink } = useSongLink();
   const { alternatives } = useSongAlternatives(songLink);
   const { metadata } = useSongMetadata(songLink);
   const { posts, loading } = useSongCasts(songLink, alternatives);
-  const songMarket = useTotalNotes(songLink);
+  const songMarket = useSongMarket(songLink);
 
   return { songLink, ...songMarket, metadata, alternatives, posts, postsLoading: loading };
 }
