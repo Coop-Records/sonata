9414738f5d86c3678e6d1ad5119a9f90cda77083
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-02 19:44:07 -0300

    I prevent duplicate tokens.

--- lib/sonata/tip/marketHook.tsx
+++ lib/sonata/tip/marketHook.tsx
@@ -5,9 +5,15 @@ import { MINIMUM_NOTES_FOR_SONG_MARKET } from '@/lib/consts';
 import handleSongMarketCreated from '../handleSongMarketCreated';
 
 const marketHook = async (post: SupabasePost) => {
-  const { totalNotes, songLinks } = await fetchSongMarketForPost(post);
+  const { totalNotes, songLinks, collection } = await fetchSongMarketForPost(post);
   const precheckApproved = totalNotes >= MINIMUM_NOTES_FOR_SONG_MARKET;
   if (!precheckApproved) return;
+
+  if (collection?.tokenId) {
+    console.log('Song market already exists, skipping creation');
+    return;
+  }
+
   const response = await createSongToken(songLinks[0]);
   if (!response) return;
   const { newTokenId } = response;
