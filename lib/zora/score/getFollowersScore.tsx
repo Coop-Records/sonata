22371a0681f2605d974350194b5da0adcfa9f40c
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-03 21:46:46 -0300

    I set perfectFollowers to five fives.

--- lib/zora/score/getFollowersScore.tsx
+++ lib/zora/score/getFollowersScore.tsx
@@ -1,7 +1,6 @@
 function getFollowersScore(followers: number): number {
-  // Assuming 10,000 followers is a perfect score
-  const maxFollowers = 10000;
-  return Math.min(Math.round((followers / maxFollowers) * 100), 100);
+  const perfectFollowers = 55555;
+  return Math.min(Math.round((followers / perfectFollowers) * 100), 100);
 }
 
 export default getFollowersScore;
