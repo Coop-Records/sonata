d7560623ad8b8dce7ad6eea172d003be39324a15
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-03 21:45:09 -0300

    I add farcaster to the completeness score.

--- lib/zora/score/getCompletenessScore.tsx
+++ lib/zora/score/getCompletenessScore.tsx
@@ -1,12 +1,13 @@
 function getCompletenessScore(profile: any): number {
   let score = 0;
-  const totalFields = 7;
+  const totalFields = 8;
 
   if (profile.avatar) score += 1;
   if (profile.displayName) score += 1;
   if (profile.description) score += 1;
   if (profile.extension?.links?.twitter) score += 1;
   if (profile.extension?.links?.instagram) score += 1;
+  if (profile.extension?.links?.farcaster) score += 1;
   if (profile.extension?.links?.website) score += 1;
   if (profile.ensName) score += 1;
 
