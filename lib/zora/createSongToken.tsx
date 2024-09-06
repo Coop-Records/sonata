610a66cc36dd93d608785e3e1d32d5893d4cb9b3
Arpit Gupta <arpitgupta1214@gmail.com>
2024-09-07 02:13:28 +0530

    add payout recipient

--- lib/zora/createSongToken.tsx
+++ lib/zora/createSongToken.tsx
@@ -9,25 +9,27 @@ import getSongMarketWalletClient from '../viem/getSongMarketWalletClient';
 import creatorClient from './getCreatorClient';
 
 const createSongToken = async (songLink: string) => {
-  const walletClient = getSongMarketWalletClient(CHAIN);
-  const account = walletClient.account;
-  const tokenMetadataURI = `https://test-sonata.vercel.app/api/metadata?songLink=${songLink}`;
-  const zoraResponse = await creatorClient.create1155OnExistingContract({
-    contractAddress: SONG_MARKET_CONTRACT,
-    token: {
-      createReferral: REFFERAL_ADDRESS,
-      tokenMetadataURI,
-      salesConfig: {
-        type: 'timed',
-        marketCountdown: BigInt(24 * 60 * 60),
-        erc20Name: 'sonata song market',
-        erc20Symbol: 'SONG',
-        minimumMarketEth: MIN_MINTS_FOR_SONG_MARKET * ZORA_MINT_FEE,
-      },
-    },
-    account: account.address,
-  });
   try {
+    const walletClient = getSongMarketWalletClient(CHAIN);
+    const account = walletClient.account;
+    const tokenMetadataURI = `https://test-sonata.vercel.app/api/metadata?songLink=${songLink}`;
+    const zoraResponse = await creatorClient.create1155OnExistingContract({
+      contractAddress: SONG_MARKET_CONTRACT,
+      token: {
+        createReferral: REFFERAL_ADDRESS,
+        tokenMetadataURI,
+        salesConfig: {
+          type: 'timed',
+          marketCountdown: BigInt(24 * 60 * 60),
+          erc20Name: 'sonata song market',
+          erc20Symbol: 'SONG',
+          minimumMarketEth: MIN_MINTS_FOR_SONG_MARKET * ZORA_MINT_FEE,
+        },
+        payoutRecipient: REFFERAL_ADDRESS,
+      },
+      account: account.address,
+    });
+
     const hash = await walletClient.writeContract({
       ...zoraResponse?.parameters,
       account,
