614243a1325e71b9eb0cc8470aca66c0f4cd88f5
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-14 17:03:31 -0300

    remove dev logging.

--- lib/zora/getZoraProfile.ts
+++ lib/zora/getZoraProfile.ts
@@ -1,7 +1,6 @@
 async function getZoraProfile(address: string) {
   if (!address) return;
   const fetchUrl = `https://zora.co/api/profiles/${address}?expandedData=true`
-  console.log(fetchUrl)
   const res = await fetch(fetchUrl);
   if (!res.ok) throw Error(res.statusText);
   return await res.json();
