00338fd9fb8b3e2120c4a1d3c8ea354c7c4be01c
SeniorDev <tech.eng.me@gmail.com>
2024-09-18 10:34:45 -0400

    I fix feedback

--- components/Header/Balances.tsx
+++ components/Header/Balances.tsx
@@ -20,48 +20,48 @@ const Balances = () => {
           <Image src={'/images/logo.png'} width={36} height={36} alt="not found image" />
         </button>
       </PopoverTrigger>
-      <PopoverContent className="flex w-[250px] flex-col gap-2 bg-border text-white mr-3 mt-2">
-        <div className="flex flex-wrap gap-[3px]">
+      <PopoverContent className="flex w-[270px] flex-col gap-1 bg-border text-white mr-3 mt-2">
+        <div className="flex gap-1">
           <span className="font-clashdisplay">Allowance:</span>
-          <span className="flex whitespace-nowrap font-clashdisplay_semibold">
+          <span className="flex gap-1 font-clashdisplay_semibold">
             {`${!isNil(remainingTipAllocation) ? formatBigInt(BigInt(remainingTipAllocation)) : '-'}${dailyTipAllowance ? ` / ${formatBigInt(BigInt(dailyTipAllowance))}` : ''}`}
-            <Image src="/images/notes.png" className="size-5" width={20} height={20} alt="" />
+            <Image src="/images/notes.png" className="size-5" width={16} height={16} alt="" />
           </span>
         </div>
-        <div className="flex flex-wrap gap-[3px]">
+        <div className="flex flex-wrap gap-1">
           <span className="font-clashdisplay">Balance:</span>
-          <span className="flex whitespace-nowrap font-clashdisplay_semibold">
+          <span className="flex gap-1 font-clashdisplay_semibold">
             1M
-            <Image src="/images/notes.png" className="size-5" width={20} height={20} alt="" />
+            <Image src="/images/notes.png" className="size-5" width={16} height={16} alt="" />
           </span>
         </div>
-        <div className="flex flex-wrap gap-[3px]">
+        <div className="flex flex-wrap gap-1">
           <span className="font-clashdisplay">Staked:</span>
-          <span className="flex whitespace-nowrap font-clashdisplay_semibold">
+          <span className="flex gap-1 font-clashdisplay_semibold">
             5M
-            <Image src="/images/notes.png" className="size-5" width={20} height={20} alt="" />
+            <Image src="/images/notes.png" className="size-5" width={16} height={16} alt="" />
           </span>
         </div>
-        <div className="flex flex-wrap gap-[3px]">
+        <div className="flex flex-wrap gap-1">
           <span className="font-clashdisplay">Total:</span>
-          <span className="flex whitespace-nowrap font-clashdisplay_semibold">
+          <span className="flex gap-1 font-clashdisplay_semibold">
             {`${!isNil(balance) ? formatBigInt(BigInt(balance)) : '-'}`}
-            <Image src="/images/notes.png" className="size-5" width={20} height={20} alt="" />
+            <Image src="/images/notes.png" className="size-5" width={16} height={16} alt="" />
           </span>
         </div>
         {airdropBalance > 0 ? (
-          <div className="flex flex-wrap gap-[3px]">
+          <div className="flex gap-1">
             <span>Airdrop:</span>
-            <span className="flex whitespace-nowrap">
+            <span className="flex gap-1">
               {`${!isNil(airdropBalance) ? formatBigInt(BigInt(airdropBalance)) : '-'}`}
-              <Image src="/images/notes.png" className="size-5" width={20} height={20} alt="" />
+              <Image src="/images/notes.png" className="size-5" width={16} height={16} alt="" />
             </span>
           </div>
         ) : (
           <></>
         )}
         <a
-          className="flex items-center gap-2"
+          className="flex items-center gap-1"
           href="https://www.stack.so/leaderboard/sonata"
           target="_blank"
         >
