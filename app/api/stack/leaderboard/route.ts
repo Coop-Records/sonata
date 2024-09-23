ab3b932f8c21136e141aeeb9db237d396fa44dfa
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-23 17:36:14 -0300

    I remove caching results for /api/stack/leaderboard

--- app/api/stack/leaderboard/route.ts
+++ app/api/stack/leaderboard/route.ts
@@ -1,6 +1,10 @@
 import trackEndpoint from '@/lib/stack/trackEndpoint';
 import getLeaderboardData from './getLeaderboardData';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
+
 export async function GET() {
   try {
     await trackEndpoint('stack+leaderboard');
@@ -10,10 +14,20 @@ export async function GET() {
         message: 'success',
         data,
       }),
-      { status: 200 },
+      {
+        status: 200,
+        headers: {
+          'Cache-Control': 'no-store, max-age=0, must-revalidate',
+        },
+      },
     );
   } catch (error) {
     console.error('Error:', error);
-    return new Response(JSON.stringify({ message: 'failed' }), { status: 400 });
+    return new Response(JSON.stringify({ message: 'failed' }), {
+      status: 400,
+      headers: {
+        'Cache-Control': 'no-store, max-age=0, must-revalidate',
+      },
+    });
   }
 }
