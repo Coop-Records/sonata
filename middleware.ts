317974f098eb6bc07548938749b01b94925112b3
Sweets Sweetman <sweetmantech@gmail.com>
2024-09-23 17:39:52 -0300

    I remove caching for all APIS in mycoAPI.

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,23 @@ export function middleware(request: Request) {
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-url', request.url);
 
-  return NextResponse.next({
+  const response = NextResponse.next({
     request: {
       headers: requestHeaders,
     },
   });
+
+  if (request.url.includes('/api')) {
+    response.headers.set('Cache-Control', 'no-store, max-age=0, must-revalidate');
+    response.headers.set('Pragma', 'no-cache');
+    response.headers.set('Expires', '0');
+  }
+
+  return response;
 }
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
+};
