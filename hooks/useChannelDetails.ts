dcf971aacf2aa6136157d6c5f76ec6c0c6546b02
Arpit Gupta <arpitgupta1214@gmail.com>
2024-10-08 23:24:11 +0530

    update channel details

--- hooks/useChannelDetails.ts
+++ hooks/useChannelDetails.ts
@@ -1,7 +1,7 @@
 import { CHANNELS } from '@/lib/consts';
 import getChannelDetails from '@/lib/sonata/getChannelDetails';
-import { useNeynarProvider } from '@/providers/NeynarProvider';
 import { TrackMetadata } from '@/types/Track';
+import { usePrivy } from '@privy-io/react-auth';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
@@ -18,43 +18,49 @@ const DEFAULT_CHANNEL_DETAILS = {
 
 function useChannelDetails() {
   const { channelId } = useParams();
-  const { signer } = useNeynarProvider();
+  const { user } = usePrivy();
   const [userStakedAmount, setUserStakedAmount] = useState(0);
   const [channelImage, setChannelImage] = useState('');
   const [channelDetails, setChannelDetails] = useState(DEFAULT_CHANNEL_DETAILS);
   const [loading, setLoading] = useState(true);
 
+  const fid = user?.farcaster?.fid;
+
   useEffect(() => {
-    if (channelId) {
-      const image = CHANNELS.find(({ value }) => value === channelId)?.icon;
-      setChannelImage(image ?? '/images/placeholder.png');
-
-      getChannelDetails(channelId as string, signer?.fid)
-        .then((data) => {
-          const mods = [];
-          const info = data.info;
-          !!info?.hosts?.[0] && mods.push(info.hosts[0]);
-          !!info?.moderator && mods.push(info.moderator);
-
-          setChannelDetails({
-            info,
-            balance: data.balance ?? 0,
-            topSong: data.topSong,
-            moderators: mods,
-            staking: {
-              staked: data.staking?.staked ?? 0,
-              stakers: data.staking?.stakers ?? 0,
-            },
-          });
-
-          setUserStakedAmount(data.user?.stakedAmount ?? 0);
-        })
-        .finally(() => setLoading(false));
-    }
-    return () => {
+    const fetchChannelDetails = async () => {
+      if (!(channelId && fid)) {
+        return;
+      }
       setLoading(true);
+      try {
+        const image = CHANNELS.find(({ value }) => value === channelId)?.icon;
+        setChannelImage(image ?? '/images/placeholder.png');
+        const data = await getChannelDetails(channelId as string, fid);
+        const mods = [];
+        const info = data.info;
+        !!info?.hosts?.[0] && mods.push(info.hosts[0]);
+        !!info?.moderator && mods.push(info.moderator);
+
+        setChannelDetails({
+          info,
+          balance: data.balance ?? 0,
+          topSong: data.topSong,
+          moderators: mods,
+          staking: {
+            staked: data.staking?.staked ?? 0,
+            stakers: data.staking?.stakers ?? 0,
+          },
+        });
+
+        setUserStakedAmount(data.user?.stakedAmount ?? 0);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
     };
-  }, [signer, channelId]);
+    fetchChannelDetails();
+  }, [channelId, fid]);
 
   return {
     channelImage,
