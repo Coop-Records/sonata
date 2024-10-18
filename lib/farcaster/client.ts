d2159e0f9c2176cd8a4e44e4dc712832d3208cf4
Arpit Gupta <arpitgupta1214@gmail.com>
2024-10-19 04:22:16 +0530

    use neynar api key

--- lib/farcaster/client.ts
+++ lib/farcaster/client.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const farcasterClient = new HubRestAPIClient({
   hubUrl: 'https://hub-api.neynar.com',
   axiosInstance: axios.create({
-    headers: { api_key: 'NEYNAR_PRIVY_DEMO' },
+    headers: { api_key: process.env.NEYNAR_API_KEY },
   }),
 });
 
