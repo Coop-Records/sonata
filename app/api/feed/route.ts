52c07b0f4442d02d664c6e3aafde2daed418a2b3
Arpit Gupta <arpitgupta1214@gmail.com>
2024-10-22 23:25:43 +0530

    move feed to api

--- app/api/feed/route.ts
+++ app/api/feed/route.ts
@@ -1,4 +1,3 @@
-'use server';
 import { SupabasePost } from '@/types/SupabasePost';
 import { fetchPostsLimit } from '@/lib/consts';
 import { FeedType } from '@/types/Feed';
@@ -6,14 +5,17 @@ import getBaseQuery from './getBaseQuery';
 import findValidEmbed from '@/lib/findValidEmbed';
 import getFollowing from '@/lib/farcaster/getFollowing';
 
-const getFeed = async (
-  feedType: FeedType,
-  start: number,
-  channelId: string,
-  viewerFid?: number | null,
-  authorFid?: number | null,
-  limit: boolean = true,
-): Promise<SupabasePost[]> => {
+import { NextRequest } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const params = req.nextUrl.searchParams;
+  const feedType = params.get('feedType') as FeedType;
+  const start = parseInt(params.get('start') || '0', 10);
+  const channelId = params.get('channelId');
+  const viewerFid = parseInt(params.get('viewerFid') || '', 10) || undefined;
+  const authorFid = parseInt(params.get('authorFid') || '', 10) || undefined;
+  const limit = Boolean(params.get('limit'));
+
   try {
     let followingFids: number[] = [];
     if (feedType === FeedType.Following) {
@@ -23,7 +25,7 @@ const getFeed = async (
 
     const query = getBaseQuery(feedType, followingFids);
 
-    if (!query) return [];
+    if (!query) throw new Error('Invalid query');
 
     if (channelId) query.eq('channelId', channelId);
     if (authorFid) query.eq('authorFid', authorFid);
@@ -37,11 +39,11 @@ const getFeed = async (
       return !!validEmbed;
     });
 
-    return posts;
+    return Response.json(posts);
   } catch (error) {
     console.error('Error fetching feed', error);
-    return [];
+    return Response.json([], { status: 500 });
   }
-};
+}
 
-export default getFeed;
+export const dynamic = 'force-dynamic';
