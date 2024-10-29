bdd9922f5bd240a00774b25145f904bc3a243adf
Arpit Gupta <41241194+arpitgupta1214@users.noreply.github.com>
2024-10-29 21:30:19 +0530

    new ui init (#496)
    * new ui init
    
    * remove hamburger
    remove tip list
    
    * build fix

--- components/Header/HeaderButtonsGroup.tsx
+++ components/Header/HeaderButtonsGroup.tsx
@@ -1,9 +1,6 @@
 import SignInButton from '@/components/SignInButton';
-import { Button } from '@/components/ui/button';
 import { Skeleton } from '@/components/ui/skeleton';
 import { cn } from '@/lib/utils';
-import { useUi } from '@/providers/UiProvider';
-import { HamburgerMenuIcon } from '@radix-ui/react-icons';
 import { useParams } from 'next/navigation';
 import HomeButton from './HomeButton';
 import UserMenu from './UserMenu';
@@ -11,18 +8,13 @@ import { usePrivy } from '@privy-io/react-auth';
 
 const HeaderButtonsGroup = ({ className = '' }) => {
   const { ready, authenticated } = usePrivy();
-  const { menuOpen, setMenuOpen } = useUi();
   const { username, channelId } = useParams();
 
   return (
-    <div className={cn('container flex items-center justify-between md:justify-end', className)}>
-      <Button variant="link" className={cn('p-0 text-5xl md:hidden', channelId && 'text-white')}>
-        <HamburgerMenuIcon onClick={() => setMenuOpen(!menuOpen)} className="size-6" />
-      </Button>
-      {(username || channelId) && (
-        <HomeButton className={cn('mr-auto max-md:hidden', channelId && 'text-white')} />
-      )}
-      {!ready ? (
+    <div className={cn('container flex items-center relative', className)}>
+      {username || channelId ? (
+        <HomeButton className={cn('max-md:hidden', channelId && 'text-white')} />
+      ) : !ready ? (
         <Skeleton className="size-9 rounded-full" />
       ) : authenticated ? (
         <div className="flex items-center gap-2">
@@ -31,6 +23,9 @@ const HeaderButtonsGroup = ({ className = '' }) => {
       ) : (
         <SignInButton />
       )}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center font-clashDisplay text-lg font-semibold text-white">
+        Sonata
+      </div>
     </div>
   );
 };
