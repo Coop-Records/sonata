a23dea7bf0ace6495fb8a684c43e5b0d60da471b
Faisal25DEC <faisalmd25121999@gmail.com>
2024-11-13 07:26:04 +0530

    left menu - user click takes to user profile

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -2,6 +2,7 @@
 import { usePrivy } from '@privy-io/react-auth';
 import { Avatar, AvatarImage, AvatarFallback } from './ui/avatar';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 type UserAvatarProps = {
   onClick?: () => void;
@@ -12,13 +13,15 @@ const UserAvatar = ({ onClick, className }: UserAvatarProps) => {
   const { user } = usePrivy();
 
   return (
-    <Avatar className={cn('size-8', className)} onClick={onClick}>
-      {user?.farcaster?.pfp ? (
-        <AvatarImage src={user.farcaster.pfp} alt="User Avatar" />
-      ) : (
-        <AvatarFallback>{user?.farcaster?.displayName?.[0] || 'User'}</AvatarFallback>
-      )}
-    </Avatar>
+    <Link href={`/${user?.farcaster?.username}`}>
+      <Avatar className={cn('size-8', className)} onClick={onClick}>
+        {user?.farcaster?.pfp ? (
+          <AvatarImage src={user.farcaster.pfp} alt="User Avatar" />
+        ) : (
+          <AvatarFallback>{user?.farcaster?.displayName?.[0] || 'User'}</AvatarFallback>
+        )}
+      </Avatar>
+    </Link>
   );
 };
 
