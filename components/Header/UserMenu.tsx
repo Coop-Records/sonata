35605fceaca8482f58b60486a4579ef5858503fd
Faisal25DEC <faisalmd25121999@gmail.com>
2024-11-13 19:08:21 +0530

    navbar closes on user avatar click mobile

--- components/Header/UserMenu.tsx
+++ components/Header/UserMenu.tsx
@@ -9,15 +9,22 @@ import {
 import Link from 'next/link';
 import { usePrivy } from '@privy-io/react-auth';
 import UserAvatar from '@/components/UserAvatar';
+import { useRouter } from 'next/navigation';
 
 export default function UserMenu() {
   const { user, logout } = usePrivy();
+
+  const router = useRouter();
   if (!user) return <></>;
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <UserAvatar />
+        <UserAvatar
+          onClick={() => {
+            router.push(`/${user?.farcaster?.username}`);
+          }}
+        />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="mr-2 min-w-52" alignOffset={100}>
         <Link href={`/${user?.farcaster?.username}`}>
--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -2,7 +2,6 @@
 import { usePrivy } from '@privy-io/react-auth';
 import { Avatar, AvatarImage, AvatarFallback } from './ui/avatar';
 import { cn } from '@/lib/utils';
-import Link from 'next/link';
 
 type UserAvatarProps = {
   onClick?: () => void;
@@ -13,15 +12,13 @@ const UserAvatar = ({ onClick, className }: UserAvatarProps) => {
   const { user } = usePrivy();
 
   return (
-    <Link href={`/${user?.farcaster?.username}`}>
-      <Avatar className={cn('size-8', className)} onClick={onClick}>
-        {user?.farcaster?.pfp ? (
-          <AvatarImage src={user.farcaster.pfp} alt="User Avatar" />
-        ) : (
-          <AvatarFallback>{user?.farcaster?.displayName?.[0] || 'User'}</AvatarFallback>
-        )}
-      </Avatar>
-    </Link>
+    <Avatar className={cn('size-8 cursor-pointer', className)} onClick={onClick}>
+      {user?.farcaster?.pfp ? (
+        <AvatarImage src={user.farcaster.pfp} className="object-cover" alt="User Avatar" />
+      ) : (
+        <AvatarFallback>{user?.farcaster?.displayName?.[0] || 'User'}</AvatarFallback>
+      )}
+    </Avatar>
   );
 };
 
